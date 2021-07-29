a846d033db94a01a386033f2ea8bfc46e0fc5e0b
Mateusz Łachacz <info@blurbyte.com>
2021-07-29 12:47:48 +0200

    chore: basic eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+// Add DataCamp eslint config after it's compatible with eslint 7
+
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint"
+  ],
+  "rules": {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  }
+};
