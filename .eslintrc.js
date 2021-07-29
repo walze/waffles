30f86343fc437cd9233edd7e15e51ea738500e3d
Mateusz Łachacz <info@blurbyte.com>
2021-07-29 13:18:07 +0200

    chore: format codebase with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,33 @@
 // Add DataCamp eslint config after it's compatible with eslint 7
 
 module.exports = {
-  "env": {
-    "browser": true,
-    "node": true,
-    "es2021": true
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier"
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 12,
-    "sourceType": "module"
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-  "plugins": [
-    "react",
-    "@typescript-eslint"
-  ],
-  "rules": {
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
     'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off'
-  }
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
 };
