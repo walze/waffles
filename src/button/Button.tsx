17ba3c574325927af85c42d7ffe5b1197002c0eb
Mateusz Łachacz <info@blurbyte.com>
2021-07-30 12:03:59 +0200

    feat: update dummy button example

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,11 +1,35 @@
 import { css } from '@emotion/react';
+import React from 'react';
 
 const buttonStyle = css`
-  background-color: red;
+  color: #05192d;
+  background-color: #03ef62;
+  height: 36px;
+  min-width: 100px;
+  font-family: Studio-Feixen-Sans, Arial, sans-serif;
+  font-weight: 800;
+  font-size: 14px;
+  line-height: 1;
+  border-radius: 4px;
+  border: 0;
+  outline: 0;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #65ff8f;
+  }
+
+  &:active {
+    transform: scale(0.975);
+  }
 `;
 
-function Button() {
-  return <button css={buttonStyle}>Hello!</button>;
+interface ButtonProps {
+  children: React.ReactNode;
+}
+
+function Button({ children }: ButtonProps) {
+  return <button css={buttonStyle}>{children}</button>;
 }
 
 export default Button;
