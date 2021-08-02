63bccdc8d3c6a16fc97f948e6b16693bc1c53bd2
Mateusz Łachacz <info@blurbyte.com>
2021-08-02 14:20:56 +0200

    feat: setup playground entry

---
+++ playground/pages/index.tsx
@@ -0,0 +1,47 @@
+import Head from 'next/head';
+
+import Main from '../components/main';
+
+import { Button } from '@datacamp/waffles/button';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Waffles 2.0 Playground</title>
+        <meta
+          name="description"
+          content="Interactive playground to develop and polish Waffles components."
+        />
+        <link href="/favicon.ico" rel="icon" />
+        <link href="/icon.svg" rel="icon" type="image/svg+xml" />
+        <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
+        <link href="/manifest.webmanifest" rel="manifest" />
+        <link
+          as="font"
+          crossOrigin="anonymous"
+          href="/fonts/StudioFeixenSansRegular.woff2"
+          rel="preload"
+          type="font/woff2"
+        />
+        <link
+          as="font"
+          crossOrigin="anonymous"
+          href="/fonts/StudioFeixenSansBold.woff2"
+          rel="preload"
+          type="font/woff2"
+        />
+        <link
+          as="font"
+          crossOrigin="anonymous"
+          href="/fonts/JetBrainsMono.woff2"
+          rel="preload"
+          type="font/woff2"
+        />
+      </Head>
+      <Main>
+        <Button>Click!</Button>
+      </Main>
+    </div>
+  );
+}
