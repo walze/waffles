41a86ad1a1cae5c7537a8c5b32646086af197eb1
Mateusz Łachacz <info@blurbyte.com>
2021-08-06 11:04:28 +0200

    chore: configure release-it

--- .release-it.js
+++ .release-it.js
@@ -1,3 +1,39 @@
 module.exports = {
-  npm: false,
+  hooks: {
+    'after:bump': 'build:copy-files',
+  },
+  git: {
+    tagName: 'v${version}',
+    commitMessage: 'chore: v${version} release',
+  },
+  github: {
+    release: true,
+    releaseName: '${version}',
+  },
+  npm: {
+    publish: true,
+    publishPath: './dist',
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      infile: 'CHANGELOG.md',
+      preset: {
+        name: 'conventionalcommits',
+        types: [
+          {
+            type: 'feat',
+            section: 'Features',
+          },
+          {
+            type: 'fix',
+            section: 'Bug Fixes',
+          },
+          {
+            type: 'perf',
+            section: 'Performance Improvements',
+          },
+        ],
+      },
+    },
+  },
 };
