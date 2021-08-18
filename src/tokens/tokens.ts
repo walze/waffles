c9dd36c3c675c7781b9256d7d638c6a36582c928
Mateusz Łachacz <info@blurbyte.com>
2021-08-18 19:01:31 +0200

    feat(tokens): implement design tokens

---
+++ src/tokens/tokens.ts
@@ -0,0 +1,33 @@
+import sizing from './sizing';
+import spacing from './spacing';
+import colors from './colors';
+import borderRadius from './border-radius';
+import borderWidth from './border-width';
+import boxShadow from './box-shadow';
+import fontWeights from './font-weights';
+import fontFamilies from './font-families';
+import fontSizes from './font-sizes';
+import letterSpacing from './letter-spacing';
+import lineHeights from './line-heights';
+import opacity from './opacity';
+import zIndex from './z-index';
+import breakpoints from './breakpoints';
+
+const tokens = {
+  sizing,
+  spacing,
+  colors,
+  borderRadius,
+  borderWidth,
+  boxShadow,
+  fontWeights,
+  fontFamilies,
+  fontSizes,
+  letterSpacing,
+  lineHeights,
+  opacity,
+  zIndex,
+  breakpoints,
+};
+
+export default tokens;
