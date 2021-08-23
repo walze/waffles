b4ddb0963be038128e380d8fafad0ca6053ea021
Mateusz Łachacz <info@blurbyte.com>
2021-08-23 10:09:02 +0200

    chore: change names from raw to svg

--- tools/generate-icons.js
+++ tools/generate-icons.js
@@ -9,7 +9,7 @@ const prettierConfig = prettier.resolveConfig.sync(__dirname);
 const iconsDirPath = path.resolve(__dirname, '../src/icon');
 const iconsExportPath = path.join(iconsDirPath, 'index.ts');
 
-// Generate pascal case component name based on raw icon file name
+// Generate pascal case component name based on SVG icon file name
 function formattedComponentName(filename) {
   return filename
     .split('-')
@@ -19,15 +19,15 @@ function formattedComponentName(filename) {
     .join('');
 }
 
-// Extracts viewBox from raw icon
-function iconViewBox(rawIconContent) {
-  return rawIconContent.match(/viewBox="([^"]+)"/)[1];
+// Extracts viewBox from SVG icon
+function iconViewBox(svgIcon) {
+  return svgIcon.match(/viewBox="([^"]+)"/)[1];
 }
 
 // Grab pretty much everything between SVG tags
 // Transofrm some attributes to ones compatible with React
-function rawIconInnerContent(rawIconContent) {
-  return rawIconContent
+function svgIconPaths(svgIcon) {
+  return svgIcon
     .replace(/<!--.*-->\n/gm, '')
     .replace(/<\/?svg[^>]*>/gm, '')
     .replace(/^\s*\n/gm, '')
@@ -40,24 +40,24 @@ function rawIconInnerContent(rawIconContent) {
 }
 
 // Generate React component based on provided SVG content
-function componentContent(componentName, rawIconContent) {
+function componentFromSvg(componentName, svgIcon) {
   return `function ${componentName}() { return <svg
-    viewBox="${iconViewBox(rawIconContent)}"
+    viewBox="${iconViewBox(svgIcon)}"
     width="18"
     height="18">
-    ${rawIconInnerContent(rawIconContent)}</svg> }\n
+    ${svgIconPaths(svgIcon)}</svg> }\n
     export default ${componentName}`;
 }
 
 function generateIcons() {
   // Array of SVG icons file names
-  const rawIcons = glob.sync('*.svg', { cwd: 'src/icon/raw/' });
+  const svgIcons = glob.sync('*.svg', { cwd: 'src/icon/raw/' });
 
   // Array of export statements
   const iconsExports = [];
 
-  rawIcons.forEach((rawIconFilename) => {
-    const filename = rawIconFilename.split('.')[0];
+  svgIcons.forEach((svgFilename) => {
+    const filename = svgFilename.split('.')[0];
     const componentName = formattedComponentName(filename);
 
     iconsExports.push(
@@ -65,14 +65,14 @@ function generateIcons() {
     );
 
     // Grab the whole content of SVG file
-    const rawIconContent = fs.readFileSync(
-      path.join(iconsDirPath, 'raw', rawIconFilename),
+    const svgIcon = fs.readFileSync(
+      path.join(iconsDirPath, 'raw', svgFilename),
       { encoding: 'utf-8' },
     );
 
     // Generate and format new React component content
     const formattedContent = prettier.format(
-      componentContent(componentName, rawIconContent),
+      componentFromSvg(componentName, svgIcon),
       {
         parser: 'typescript',
         ...prettierConfig,
