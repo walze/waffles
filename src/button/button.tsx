4d501b146857748322b920e80064fce88387cb6a
Mateusz Łachacz <info@blurbyte.com>
2021-08-25 13:07:17 +0200

    feat(button): implement base sizes and variants

--- src/button/button.tsx
+++ src/button/button.tsx
@@ -1,39 +1,85 @@
 import React from 'react';
 import { css } from '@emotion/react';
+import { transparentize } from 'polished';
 
 import { tokens } from '../tokens';
 
+const sizeMap = {
+  small: tokens.sizing.small,
+  medium: tokens.sizing.medium,
+  large: tokens.sizing.large,
+};
+
+const variantMap = {
+  primary: {
+    color: tokens.colors.navy,
+    backgroundColor: tokens.colors.green,
+    hoverColor: tokens.colors.greenLight,
+    borderColor: 'transparent',
+  },
+  secondary: {
+    color: tokens.colors.navy,
+    backgroundColor: 'transparent',
+    hoverColor: transparentize(1 - tokens.opacity.low, tokens.colors.navy),
+    borderColor: tokens.colors.navy,
+  },
+  plain: {
+    color: tokens.colors.blueDarkText,
+    backgroundColor: 'transparent',
+    hoverColor: transparentize(1 - tokens.opacity.low, tokens.colors.navy),
+    borderColor: 'transparent',
+  },
+  destructive: {
+    color: tokens.colors.navy,
+    backgroundColor: tokens.colors.red,
+    hoverColor: tokens.colors.redLight,
+    borderColor: 'transparent',
+  },
+};
+
 const buttonStyle = css`
-  color: ${tokens.colors.navy};
-  background-color: ${tokens.colors.green};
-  height: ${tokens.sizing.medium};
-  min-width: 110px;
   font-family: ${tokens.fontFamilies.sansSerif};
   font-weight: ${tokens.fontWeights.bold};
   font-size: ${tokens.fontSizes.medium};
   line-height: ${tokens.lineHeights.tight};
   outline: 0;
   border-radius: ${tokens.borderRadius.medium};
-  border: 0;
+  border-width: ${tokens.borderWidth.medium};
   cursor: pointer;
-
-  &:hover {
-    background-color: ${tokens.colors.greenLight};
-  }
-
-  &:active {
-    transform: scale(0.975);
-  }
+  transition: background-color 125ms ease-out;
 `;
 
 type ButtonProps = {
+  variant?: 'primary' | 'secondary' | 'plain' | 'destructive';
+  size?: 'small' | 'medium' | 'large';
   children: React.ReactNode;
-  onClick?: (event?: React.MouseEvent<HTMLButtonElement>) => void;
-};
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-function Button({ children, onClick }: ButtonProps) {
+function Button({
+  variant = 'primary',
+  size = 'medium',
+  children,
+  ...restProps
+}: ButtonProps) {
   return (
-    <button onClick={onClick} css={buttonStyle}>
+    <button
+      css={css`
+        ${buttonStyle}
+        height: ${sizeMap[size]};
+        color: ${variantMap[variant].color};
+        background-color: ${variantMap[variant].backgroundColor};
+        border-color: ${variantMap[variant].borderColor};
+
+        &:hover {
+          background-color: ${variantMap[variant].hoverColor};
+        }
+
+        &:active {
+          background-color: ${variantMap[variant].backgroundColor};
+        }
+      `}
+      {...restProps}
+    >
       {children}
     </button>
   );
