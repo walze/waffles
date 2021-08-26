8e191eca53ea9207a52b92ae7248d1331b3f43d8
Mateusz Łachacz <info@blurbyte.com>
2021-08-26 12:48:21 +0200

    feat(utils): implement hexToRgba color conversion util

---
+++ src/utils/hex-to-rgba.ts
@@ -0,0 +1,20 @@
+const hexRegex = /^#([a-fA-F0-9]{3}){1,2}$/;
+
+// Convert HEX color to RGBA counterpart, with optional alpha setting
+function hexToRgba(hexColor: string, alpha = 1) {
+  if (!hexRegex.test(hexColor)) {
+    throw new Error(
+      "Couldn't parse the color string. Please provide the color as a string in HEX notation.",
+    );
+  }
+
+  const value = hexColor.slice(1);
+  const group = value.length / 3;
+  return `rgba(${[0, 1, 2]
+    .map((i) => value.slice(i * group, (i + 1) * group))
+    .map((v) => parseInt((v + v).slice(-2), 16).toString())
+    .concat(alpha.toString())
+    .join(', ')})`;
+}
+
+export default hexToRgba;
