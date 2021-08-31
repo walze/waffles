6884a47d469634dbd587afeb291163e2bab201bd
Mateusz Łachacz <info@blurbyte.com>
2021-08-31 14:23:47 +0200

    test: add full width button stories

---
+++ src/button/__tests__/button-full-width.story.tsx
@@ -0,0 +1,46 @@
+import { css } from '@emotion/react';
+
+import { tokens } from '../../tokens';
+import { AddCircle } from '../../icon';
+import { Button } from '../index';
+
+const wrapperStyle = css`
+  display: flex;
+  flex-wrap: wrap;
+  gap: ${tokens.spacing.medium};
+  padding: ${tokens.spacing.medium};
+`;
+
+const variants = ['primary', 'secondary', 'plain', 'destructive'] as const;
+
+function Story() {
+  return (
+    <>
+      <div css={wrapperStyle}>
+        {variants.map((variant) => {
+          return (
+            <Button key={`${variant}`} variant={variant} fullWidth>
+              Full Width
+            </Button>
+          );
+        })}
+      </div>
+      <div css={wrapperStyle}>
+        {variants.map((variant) => {
+          return (
+            <Button
+              iconLeft={AddCircle}
+              key={`${variant}`}
+              variant={variant}
+              fullWidth
+            >
+              With Icon
+            </Button>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default Story;
