c99c7b8e600cb138dec00af1dbb4dd13a5749343
Mateusz Łachacz <info@blurbyte.com>
2021-08-31 14:24:22 +0200

    test: add button with icons stories

---
+++ src/button/__tests__/button-with-icons.story.tsx
@@ -0,0 +1,89 @@
+import { css } from '@emotion/react';
+
+import { tokens } from '../../tokens';
+import { AddCircle, ChevronLeft, ChevronRight } from '../../icon';
+import { Button } from '../index';
+
+const wrapperStyle = css`
+  display: flex;
+  flex-wrap: wrap;
+  gap: ${tokens.spacing.medium};
+  padding: ${tokens.spacing.medium};
+`;
+
+const variants = ['primary', 'secondary', 'plain', 'destructive'] as const;
+const sizes = ['small', 'medium', 'large'] as const;
+
+function Story() {
+  return (
+    <>
+      {/* Regular */}
+      <div css={wrapperStyle}>
+        {variants.map((variant) => {
+          return sizes.map((size) => {
+            return (
+              <Button
+                key={`only-icon-${variant}-${size}`}
+                icon={AddCircle}
+                variant={variant}
+                size={size}
+                aria-label="Only icon"
+              />
+            );
+          });
+        })}
+      </div>
+      <div css={wrapperStyle}>
+        {variants.map((variant) => {
+          return sizes.map((size) => {
+            return (
+              <Button
+                key={`icon-left-${variant}-${size}`}
+                iconLeft={ChevronLeft}
+                variant={variant}
+                size={size}
+              >
+                Icon Left
+              </Button>
+            );
+          });
+        })}
+      </div>
+      <div css={wrapperStyle}>
+        {variants.map((variant) => {
+          return sizes.map((size) => {
+            return (
+              <Button
+                key={`icon-right-${variant}-${size}`}
+                iconRight={ChevronRight}
+                variant={variant}
+                size={size}
+              >
+                Icon Right
+              </Button>
+            );
+          });
+        })}
+      </div>
+      <div css={wrapperStyle}>
+        {variants.map((variant) => {
+          return sizes.map((size) => {
+            return (
+              <Button
+                key={`icon-left-and-right-${variant}-${size}`}
+                iconRight={AddCircle}
+                iconLeft={ChevronLeft}
+                variant={variant}
+                size={size}
+              >
+                Icon Left and Right
+              </Button>
+            );
+          });
+        })}
+      </div>
+    </>
+  );
+}
+
+export default Story;
