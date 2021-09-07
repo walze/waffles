b303ffd35a2a4c443b3e005523bee9d0767bca98
Mateusz Łachacz <info@blurbyte.com>
2021-09-07 16:06:45 +0200

    feat(code): make code work as pre tag

--- src/code/code.tsx
+++ src/code/code.tsx
@@ -1,13 +1,21 @@
 import { codeStyle } from './styles';
 
 type CodeProps = {
+  as?: 'code' | 'pre';
   children: React.ReactNode;
   size?: 'medium' | 'large';
   inverted?: boolean;
 } & React.HTMLAttributes<HTMLElement>;
 
-function Code({ size = 'medium', inverted = false, ...restProps }: CodeProps) {
-  return <code css={codeStyle({ size, inverted })} {...restProps} />;
+function Code({
+  as,
+  size = 'medium',
+  inverted = false,
+  ...restProps
+}: CodeProps) {
+  const Element = as || 'code';
+
+  return <Element css={codeStyle({ size, inverted })} {...restProps} />;
 }
 
 export default Code;
