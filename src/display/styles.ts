3353f6dc7f0529abc482b8e261c120aa2ca1e197
Mateusz Łachacz <info@blurbyte.com>
2021-09-07 12:33:45 +0200

    feat(display): adjust font weight

--- src/display/styles.ts
+++ src/display/styles.ts
@@ -5,6 +5,7 @@ import { tokens } from '../tokens';
 export function displayStyle() {
   return css`
     font-size: ${tokens.fontSizes.huge};
+    font-weight: ${tokens.fontWeights.bold};
     line-height: ${tokens.lineHeights.default};
     letter-spacing: ${tokens.letterSpacing.tight};
   `;
