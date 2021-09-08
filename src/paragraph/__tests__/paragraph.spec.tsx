39f08515ee5e9f2048a145243a3aff9d6a5156ab
Mateusz Łachacz <info@blurbyte.com>
2021-09-08 12:15:49 +0200

    test: add unit and snapshot paragraph tests

---
+++ src/paragraph/__tests__/paragraph.spec.tsx
@@ -0,0 +1,58 @@
+import { render } from '@testing-library/react';
+
+import { Paragraph } from '../index';
+
+const variants = ['primary', 'secondary'] as const;
+
+describe('Paragraph', () => {
+  it('renders a paragraph containing the text', () => {
+    const { container } = render(
+      <Paragraph>Short paragraph about Taylor Swift.</Paragraph>,
+    );
+
+    const paragraph = container.querySelector('p');
+
+    expect(paragraph).toBeInTheDocument();
+    expect(paragraph).toHaveTextContent('Short paragraph about Taylor Swift.');
+  });
+
+  it('sets the data attribute on the paragraph', () => {
+    const { getByTestId } = render(
+      <Paragraph data-testid="paragraph-22">
+        Short paragraph about Taylor Swift.
+      </Paragraph>,
+    );
+
+    const paragraph = getByTestId('paragraph-22');
+
+    expect(paragraph).toBeInTheDocument();
+  });
+
+  describe('renders snapshot of', () => {
+    variants.forEach((variant) => {
+      it(`variant ${variant}`, () => {
+        const { container } = render(
+          <Paragraph variant={variant}>Test</Paragraph>,
+        );
+
+        const paragraph = container.querySelector('p');
+        expect(paragraph).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('renders snapshot of inverted', () => {
+    variants.forEach((variant) => {
+      it(`variant ${variant}`, () => {
+        const { container } = render(
+          <Paragraph variant={variant} inverted>
+            Test
+          </Paragraph>,
+        );
+
+        const paragraph = container.querySelector('p');
+        expect(paragraph).toMatchSnapshot();
+      });
+    });
+  });
+});
