e9a619e355e5b91c02ccdbe8e6dc80978c958bf0
Mateusz Łachacz <info@blurbyte.com>
2021-09-08 14:31:49 +0200

    test: add chapeau unit and snapshot tests

---
+++ src/chapeau/__tests__/chapeau.spec.tsx
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react';
+
+import { Chapeau } from '../index';
+
+describe('Chapeau', () => {
+  it('renders a paragraph containing the text', () => {
+    const { container } = render(<Chapeau>Taylor Swift Anniversary</Chapeau>);
+
+    const chapeau = container.querySelector('p');
+
+    expect(chapeau).toBeInTheDocument();
+    expect(chapeau).toHaveTextContent('Taylor Swift Anniversary');
+  });
+
+  it('renders snapshot', () => {
+    const { getByText } = render(<Chapeau>Test</Chapeau>);
+
+    const chapeau = getByText('Test');
+
+    expect(chapeau).toBeInTheDocument();
+    expect(chapeau).toMatchSnapshot();
+  });
+});
