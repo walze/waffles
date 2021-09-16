bd1f0b34dc3d0b98af970e23d858dde639998360
Mateusz Łachacz <info@blurbyte.com>
2021-09-16 15:32:43 +0200

    feat(portal): implement isomorphic portal component

---
+++ src/portal/portal.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+type PortalProps = {
+  children: React.ReactNode;
+};
+
+function Portal({ children }: PortalProps) {
+  const element = useRef<HTMLElement | null>(null);
+  const [isMounted, setIsMounted] = useState(false);
+  const [, forceUpdate] = useState(Object.create(null));
+
+  // Check wheter component is mounted or not, since useEffect doesn't trigger on server
+  // Therefore all DOM objects and methods will be available
+  useEffect(() => {
+    setIsMounted(true);
+
+    return () => {
+      setIsMounted(false);
+    };
+  }, []);
+
+  // Create portal node in document's body
+  useEffect(() => {
+    if (isMounted) {
+      element.current = document.createElement('div');
+      document.body.appendChild(element.current);
+      // Simple trick to force re-render
+      forceUpdate(Object.create(null));
+    }
+
+    return () => {
+      if (isMounted && element.current) {
+        document.body.removeChild(element.current);
+      }
+    };
+  }, [isMounted]);
+
+  return element.current ? createPortal(children, element.current) : null;
+}
+
+export default Portal;
