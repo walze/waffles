e245145993e9dc4f7936911912e1348a09539ec2
Mateusz Łachacz <info@blurbyte.com>
2021-09-29 10:23:48 +0200

    docs: enable docs written in markdown with mdx

--- doc-site/next.config.js
+++ doc-site/next.config.js
@@ -6,12 +6,18 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  pageExtensions: ['ts', 'tsx', 'mdx'],
   webpack: (config, { isServer }) => {
     config.resolve.alias['@datacamp/waffles'] = resolve(__dirname, '../dist');
 
     if (!isServer) {
       config.resolve.fallback.fs = false;
     }
+
+    config.module.rules.push({
+      test: /\.mdx$/,
+      use: [{ loader: 'xdm/webpack.cjs', options: {} }],
+    });
 
     return config;
   },
