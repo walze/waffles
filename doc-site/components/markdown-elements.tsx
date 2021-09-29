28a37734d77a66a44ef21c32dd4c6f41bd9d1a73
Mateusz Łachacz <info@blurbyte.com>
2021-09-29 10:24:39 +0200

    docs: custom components mapping for mdx

---
+++ doc-site/components/markdown-elements.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+
+import { Code } from '@datacamp/waffles/code';
+import { CodeBlock } from '@datacamp/waffles/code-block';
+import { Heading } from '@datacamp/waffles/heading';
+import { Paragraph } from '@datacamp/waffles/paragraph';
+import { Link as LinkBase } from '@datacamp/waffles/link';
+
+// Use custom Waffles components inside markdown
+
+type TextProps = {
+  children: React.ReactNode;
+};
+
+function H1({ children }: TextProps) {
+  return <Heading size="xxlarge">{children}</Heading>;
+}
+
+function H2({ children }: TextProps) {
+  return <Heading size="xlarge">{children}</Heading>;
+}
+
+function H3({ children }: TextProps) {
+  return <Heading size="large">{children}</Heading>;
+}
+
+function H4({ children }: TextProps) {
+  return <Heading size="medium">{children}</Heading>;
+}
+
+type RegularLinkProps = {
+  children: React.ReactNode;
+  href: string;
+};
+
+function RegularLink({ children, href }: RegularLinkProps) {
+  return (
+    <Link href={href} passHref>
+      <LinkBase>{children}</LinkBase>
+    </Link>
+  );
+}
+
+export default {
+  h1: H1,
+  h2: H2,
+  h3: H3,
+  h4: H4,
+  p: Paragraph,
+  a: RegularLink,
+  code: CodeBlock,
+  inlineCode: Code,
+};
--- doc-site/pages/_app.tsx
+++ doc-site/pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css';
+
 import type { AppProps } from 'next/app';
 
+import MarkdownElements from '../components/markdown-elements';
+
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} components={MarkdownElements} />;
 }
+
 export default App;
