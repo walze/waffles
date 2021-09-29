4cc92e8e86e8a9b42b6ca13a64683f6a432db646
Mateusz Łachacz <info@blurbyte.com>
2021-09-29 11:00:08 +0200

    chore: update workbench title

--- workbench/pages/index.tsx
+++ workbench/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Waffles 2.0 Workbench</title>
+        <title>Waffles - Workbench</title>
         <meta
           name="description"
           content="Interactive playground to develop and polish Waffles components."
