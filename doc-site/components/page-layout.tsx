e2e63a927de4015d67cd90ef397748485ae2c202
Mateusz Łachacz <info@blurbyte.com>
2021-10-05 14:33:21 +0200

    docs: add edit page link to the bottom of relevant pages

--- doc-site/components/page-layout.tsx
+++ doc-site/components/page-layout.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
 
 import { tokens } from '@datacamp/waffles/tokens';
+import { Button } from '@datacamp/waffles/button';
 import { ContentContainer } from '@datacamp/waffles/content-container';
+import { Edit } from '@datacamp/waffles/icon';
 import { HEADER_HEIGHT } from './constants';
 import Header from './page-header';
 import Sidebar from './sidebar';
 import Navigation from './navigation';
+
+const GITHUB_EDIT_URL =
+  'https://github.com/datacamp/waffles/edit/master/doc-site/pages';
 
 const wrapperStyle = css`
   display: flex;
@@ -19,11 +25,20 @@ const mainStyle = css`
   flex-grow: 1;
 `;
 
+const footerStyle = css`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: ${tokens.spacing.large};
+`;
+
 type PageLayoutProps = {
   children: React.ReactNode;
 };
 
 function PageLayout({ children }: PageLayoutProps) {
+  const { pathname } = useRouter();
+  const isEditable = pathname !== '/';
+
   return (
     <>
       <Header />
@@ -32,7 +47,22 @@ function PageLayout({ children }: PageLayoutProps) {
           <Navigation />
         </Sidebar>
         <main css={mainStyle}>
-          <ContentContainer>{children}</ContentContainer>
+          <ContentContainer>
+            {children}
+            {isEditable && (
+              <footer css={footerStyle}>
+                <Button
+                  as="a"
+                  href={`${GITHUB_EDIT_URL}${pathname}.mdx`}
+                  variant="plain"
+                  size="small"
+                  iconLeft={<Edit />}
+                >
+                  Edit This Page
+                </Button>
+              </footer>
+            )}
+          </ContentContainer>
         </main>
       </div>
     </>
