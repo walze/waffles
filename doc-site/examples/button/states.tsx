6e90969941e72219f135c583c41cd0d68bc73e93
Mateusz Łachacz <info@blurbyte.com>
2021-10-06 17:40:39 +0200

    docs: add button states example

---
+++ doc-site/examples/button/states.tsx
@@ -0,0 +1,20 @@
+import { css } from '@emotion/react';
+
+import { tokens } from '@datacamp/waffles/tokens';
+import { Button } from '@datacamp/waffles/button';
+
+function Example() {
+  return (
+    <div
+      css={css`
+        display: flex;
+        gap: ${tokens.spacing.small};
+      `}
+    >
+      <Button>No State</Button>
+      <Button disabled>Disabled</Button>
+    </div>
+  );
+}
+
+export default Example;
