3a177565a1dcae025eb78a4fd95cac1c2b762a30
Mateusz Łachacz <info@blurbyte.com>
2021-10-06 13:39:14 +0200

    feat(button): expose button internal only for docs purposes

---
+++ src/button/button-internal.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { useFocusRing } from '@react-aria/focus';
+import { mergeProps } from '@react-aria/utils';
+
+import type { PolymorphicRef, PolymorphicComponentProps } from '../helpers';
+import { buttonStyle, innerContentStyle } from './styles';
+
+type ButtonBaseProps = {
+  variant?: 'primary' | 'secondary' | 'plain' | 'destructive';
+  size?: 'small' | 'medium' | 'large';
+  fullWidth?: boolean;
+  inverted?: boolean;
+};
+
+type ButtonIconOnlyProps = {
+  icon: React.ReactNode;
+  children?: never;
+  iconLeft?: never;
+  iconRight?: never;
+  'aria-label': string;
+} & ButtonBaseProps;
+
+type ButtonNoIconProps = {
+  icon?: never;
+  children: React.ReactNode;
+  iconLeft?: React.ReactNode;
+  iconRight?: React.ReactNode;
+  'aria-label'?: string;
+} & ButtonBaseProps;
+
+type ButtonProps<T extends React.ElementType = 'button'> =
+  PolymorphicComponentProps<T, ButtonNoIconProps | ButtonIconOnlyProps>;
+
+function ButtonInternal<T extends React.ElementType = 'button'>(
+  {
+    as,
+    variant = 'primary',
+    size = 'medium',
+    fullWidth = false,
+    inverted = false,
+    icon,
+    iconLeft,
+    iconRight,
+    children,
+    ...restProps
+  }: ButtonProps<T>,
+  ref?: PolymorphicRef<T>,
+) {
+  const Element = as || 'button';
+
+  const { focusProps, isFocusVisible } = useFocusRing();
+
+  return (
+    <Element
+      {...mergeProps(focusProps, restProps)}
+      ref={ref}
+      css={buttonStyle({
+        size,
+        variant,
+        inverted,
+        fullWidth,
+        hasIcon: !!icon,
+        isFocusVisible,
+      })}
+    >
+      {icon}
+      {iconLeft}
+      {children && (
+        <span
+          css={innerContentStyle({
+            hasLeftIcon: !!iconLeft,
+            hasRightIcon: !!iconRight,
+          })}
+        >
+          {children}
+        </span>
+      )}
+      {iconRight}
+    </Element>
+  );
+}
+
+export default ButtonInternal;
--- src/button/button.tsx
+++ src/button/button.tsx
@@ -1,89 +1,11 @@
 import React, { forwardRef } from 'react';
-import { useFocusRing } from '@react-aria/focus';
-import { mergeProps } from '@react-aria/utils';
 
-import type { PolymorphicRef, PolymorphicComponentProps } from '../helpers';
-import { buttonStyle, innerContentStyle } from './styles';
+import ButtonInternal from './button-internal';
 
-type ButtonBaseProps = {
-  variant?: 'primary' | 'secondary' | 'plain' | 'destructive';
-  size?: 'small' | 'medium' | 'large';
-  fullWidth?: boolean;
-  inverted?: boolean;
-};
-
-type ButtonIconOnlyProps = {
-  icon: React.ReactNode;
-  children?: never;
-  iconLeft?: never;
-  iconRight?: never;
-  'aria-label': string;
-} & ButtonBaseProps;
-
-type ButtonNoIconProps = {
-  icon?: never;
-  children: React.ReactNode;
-  iconLeft?: React.ReactNode;
-  iconRight?: React.ReactNode;
-  'aria-label'?: string;
-} & ButtonBaseProps;
-
-type ButtonProps<T extends React.ElementType = 'button'> =
-  PolymorphicComponentProps<T, ButtonNoIconProps | ButtonIconOnlyProps>;
-
-type ButtonComponent = <T extends React.ElementType = 'button'>(
-  props: ButtonProps<T>,
+type ButtonComponent = (
+  props: React.ComponentProps<typeof ButtonInternal>,
 ) => JSX.Element | null;
 
-function ButtonBase<T extends React.ElementType = 'button'>(
-  {
-    as,
-    variant = 'primary',
-    size = 'medium',
-    fullWidth = false,
-    inverted = false,
-    icon,
-    iconLeft,
-    iconRight,
-    children,
-    ...restProps
-  }: ButtonProps<T>,
-  ref?: PolymorphicRef<T>,
-) {
-  const Element = as || 'button';
-
-  const { focusProps, isFocusVisible } = useFocusRing();
-
-  return (
-    <Element
-      {...mergeProps(focusProps, restProps)}
-      ref={ref}
-      css={buttonStyle({
-        size,
-        variant,
-        inverted,
-        fullWidth,
-        hasIcon: !!icon,
-        isFocusVisible,
-      })}
-    >
-      {icon}
-      {iconLeft}
-      {children && (
-        <span
-          css={innerContentStyle({
-            hasLeftIcon: !!iconLeft,
-            hasRightIcon: !!iconRight,
-          })}
-        >
-          {children}
-        </span>
-      )}
-      {iconRight}
-    </Element>
-  );
-}
-
-const Button: ButtonComponent = forwardRef(ButtonBase);
+const Button: ButtonComponent = forwardRef(ButtonInternal);
 
 export default Button;
