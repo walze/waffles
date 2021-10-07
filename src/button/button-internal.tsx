a428663d36294d40b1119ffe9dbd3e0e30e02101
Mateusz Łachacz <info@blurbyte.com>
2021-10-07 10:54:52 +0200

    docs: add inline props documentation for button

--- src/button/button-internal.tsx
+++ src/button/button-internal.tsx
@@ -6,13 +6,18 @@ import type { PolymorphicRef, PolymorphicComponentProps } from '../helpers';
 import { buttonStyle, innerContentStyle } from './styles';
 
 type ButtonBaseProps = {
+  /* Defines the variant of a button. */
   variant?: 'primary' | 'secondary' | 'plain' | 'destructive';
+  /* Defines the size of the button. In most cases default `medium` size should be used. */
   size?: 'small' | 'medium' | 'large';
+  /* Allows button to grow to the width of its container. */
   fullWidth?: boolean;
+  /* Sets the style of the button suitable for dark backrounds. */
   inverted?: boolean;
 };
 
 type ButtonIconOnlyProps = {
+  /* An icon displayed as the only content of the button. Because of that `aria-label` attribute must be specified. Could be any [icon](/components/icon) from Waffles of default `medium` size, or custom component. */
   icon: React.ReactNode;
   children?: never;
   iconLeft?: never;
@@ -22,8 +27,11 @@ type ButtonIconOnlyProps = {
 
 type ButtonNoIconProps = {
   icon?: never;
+  /* The content inside the button. Most of the time should be a plain text. */
   children: React.ReactNode;
+  /* An icon displayed to the left. Could be any [icon](/components/icon) from Waffles of default `medium` size, or custom component. */
   iconLeft?: React.ReactNode;
+  /* An icon displayed to the right. Could be any [icon](/components/icon) from Waffles of default `medium` size, or custom component. */
   iconRight?: React.ReactNode;
   'aria-label'?: string;
 } & ButtonBaseProps;
