3bbf28ca97ed4ac180b96dbf465670cb7678e9c1
Mateusz Łachacz <info@blurbyte.com>
2021-10-08 10:40:35 +0200

    test: add heading stories

--- src/heading/__tests__/heading-sizes.story.tsx
+++ src/heading/__tests__/heading-sizes.story.tsx
@@ -11,13 +11,33 @@ const sizes = ['xxlarge', 'xlarge', 'large', 'medium'] as const;
 
 function Story() {
   return (
-    <div css={wrapperStyle}>
-      {sizes.map((size) => {
-        return (
-          <Heading key={size} size={size}>{`Heading size ${size}`}</Heading>
-        );
-      })}
-    </div>
+    <>
+      {/* Regular */}
+      <div css={wrapperStyle}>
+        {sizes.map((size) => {
+          return (
+            <Heading key={size} size={size}>{`Heading size ${size}`}</Heading>
+          );
+        })}
+      </div>
+      {/* Inverted */}
+      <div
+        css={css`
+          ${wrapperStyle}
+          background-color: ${tokens.colors.navy};
+        `}
+      >
+        {sizes.map((size) => {
+          return (
+            <Heading
+              key={size}
+              size={size}
+              inverted
+            >{`Heading size ${size} inverted`}</Heading>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
--- src/heading/__tests__/heading.e2e.ts
+++ src/heading/__tests__/heading.e2e.ts
@@ -1,10 +1,10 @@
 describe('Heading', () => {
   it('render all sizes', () => {
     cy.loadStory('heading-sizes');
-    cy.get('main').find('h1').should('have.length', 1);
-    cy.get('main').find('h2').should('have.length', 1);
-    cy.get('main').find('h3').should('have.length', 1);
-    cy.get('main').find('h4').should('have.length', 1);
+    cy.get('main').find('h1').should('have.length', 2);
+    cy.get('main').find('h2').should('have.length', 2);
+    cy.get('main').find('h3').should('have.length', 2);
+    cy.get('main').find('h4').should('have.length', 2);
   });
 
   it('renders with custom styles applied', () => {
