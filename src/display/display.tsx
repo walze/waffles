0368b13d558cc8e9a918f5e0a11a4cef7fee4766
Mateusz Łachacz <info@blurbyte.com>
2021-10-08 11:01:48 +0200

    feat(display): add inverted variant

--- src/display/display.tsx
+++ src/display/display.tsx
@@ -3,10 +3,15 @@ import { displayStyle } from './styles';
 
 type DisplayProps = {
   children: React.ReactNode;
+  inverted?: boolean;
 } & React.HTMLAttributes<HTMLParagraphElement>;
 
-function Display(props: DisplayProps) {
-  return <Text as="p" css={displayStyle()} {...props} />;
+function Display({ inverted = false, children, ...restProps }: DisplayProps) {
+  return (
+    <Text as="p" css={displayStyle({ inverted })} {...restProps}>
+      {children}
+    </Text>
+  );
 }
 
 export default Display;
--- src/display/styles.ts
+++ src/display/styles.ts
@@ -2,8 +2,13 @@ import { css } from '@emotion/react';
 
 import { tokens } from '../tokens';
 
-export function displayStyle() {
+type DisplayStyleOptions = {
+  inverted: boolean;
+};
+
+export function displayStyle({ inverted }: DisplayStyleOptions) {
   return css`
+    color: ${inverted ? tokens.colors.white : tokens.colors.navy};
     font-size: ${tokens.fontSizes.huge};
     font-weight: ${tokens.fontWeights.bold};
     line-height: ${tokens.lineHeights.default};
