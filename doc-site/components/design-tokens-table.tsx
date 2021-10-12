c20253e279da8b3c9760705198f70db3213763a3
Mateusz Łachacz <info@blurbyte.com>
2021-10-12 13:50:02 +0200

    docs: implement table to display design tokens

---
+++ doc-site/components/design-tokens-table.tsx
@@ -0,0 +1,42 @@
+import { css } from '@emotion/react';
+
+import { tokens } from '@datacamp/waffles/tokens';
+import DesignTokenExample from './design-token-example';
+import Table from './table';
+
+const nameStyle = css`
+  width: 50%;
+`;
+
+type DesignTokensTableProps = {
+  tokensGroup: keyof typeof tokens;
+};
+
+function DesignTokensTable({ tokensGroup }: DesignTokensTableProps) {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <Table.HeadCell css={nameStyle}>Name</Table.HeadCell>
+          <Table.HeadCell>Value</Table.HeadCell>
+          <Table.HeadCell>Example</Table.HeadCell>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(tokens[tokensGroup]).map((entry) => {
+          const [name, value] = entry;
+
+          return (
+            <tr key={`color-${name}`}>
+              <Table.Cell>{name}</Table.Cell>
+              <Table.Cell>{value}</Table.Cell>
+              <DesignTokenExample tokensGroup={tokensGroup} value={value} />
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+}
+
+export default DesignTokensTable;
