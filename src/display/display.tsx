20d226a233b2113d7ed5c4e46f93cc42c2fbabfe
Mateusz Łachacz <info@blurbyte.com>
2021-10-18 16:45:37 +0200

    docs: add display inline props documentation

--- src/display/display.tsx
+++ src/display/display.tsx
@@ -2,7 +2,9 @@ import { Text } from '../text';
 import { displayStyle } from './styles';
 
 type DisplayProps = {
+  /* Text content to display. */
   children: React.ReactNode;
+  /* Sets the style of the display suitable for dark backgrounds. */
   inverted?: boolean;
 } & React.HTMLAttributes<HTMLParagraphElement>;
 
