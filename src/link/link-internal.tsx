3e919443c658b585b1d5b7b3f5ba019d102b1ca6
Mateusz Łachacz <info@blurbyte.com>
2021-10-18 14:52:39 +0200

    docs: add link inline props documentation

---
+++ src/link/link-internal.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useFocusRing } from '@react-aria/focus';
+import { mergeProps } from '@react-aria/utils';
+
+import type { PolymorphicRef, PolymorphicComponentProps } from '../helpers';
+import { linkStyle, innerContentStyle } from './styles';
+
+type LinkBaseProps = {
+  /* The content of the link.  */
+  children: React.ReactNode;
+  /* Sets the style of the link suitable for dark backgrounds. */
+  inverted?: boolean;
+  /* An icon displayed to the left. Could be any [icon](/components/icon) from Waffles (for smaller text sizes use `small` size) or a custom component. */
+  iconLeft?: React.ReactNode;
+  /* An icon displayed to the right. Could be any [icon](/components/icon) from Waffles (for smaller text sizes use `small` size) or a custom component. */
+  iconRight?: React.ReactNode;
+};
+
+export type LinkProps<T extends React.ElementType = 'a'> =
+  PolymorphicComponentProps<T, LinkBaseProps>;
+
+function LinkInternal<T extends React.ElementType = 'a'>(
+  {
+    as,
+    inverted = false,
+    iconLeft,
+    iconRight,
+    children,
+    ...restProps
+  }: LinkProps<T>,
+  ref?: PolymorphicRef<T>,
+) {
+  const Element = as || 'a';
+
+  const { focusProps, isFocusVisible } = useFocusRing();
+
+  return (
+    <Element
+      {...mergeProps(focusProps, restProps)}
+      ref={ref}
+      css={linkStyle({
+        inverted,
+        isFocusVisible,
+      })}
+    >
+      {iconLeft}
+      {
+        <span
+          css={innerContentStyle({
+            hasLeftIcon: !!iconLeft,
+            hasRightIcon: !!iconRight,
+          })}
+        >
+          {children}
+        </span>
+      }
+      {iconRight}
+    </Element>
+  );
+}
+
+export default LinkInternal;
--- src/link/link.tsx
+++ src/link/link.tsx
@@ -1,66 +1,12 @@
 import React, { forwardRef } from 'react';
-import { useFocusRing } from '@react-aria/focus';
-import { mergeProps } from '@react-aria/utils';
 
-import type { PolymorphicRef, PolymorphicComponentProps } from '../helpers';
-import { linkStyle, innerContentStyle } from './styles';
-
-type LinkBaseProps = {
-  children: React.ReactNode;
-  inverted?: boolean;
-  iconLeft?: React.ReactNode;
-  iconRight?: React.ReactNode;
-};
-
-type LinkProps<T extends React.ElementType = 'a'> = PolymorphicComponentProps<
-  T,
-  LinkBaseProps
->;
+import LinkInternal from './link-internal';
+import type { LinkProps } from './link-internal';
 
 type LinkComponent = <T extends React.ElementType = 'a'>(
   props: LinkProps<T>,
 ) => JSX.Element | null;
 
-function LinkBase<T extends React.ElementType = 'a'>(
-  {
-    as,
-    inverted = false,
-    iconLeft,
-    iconRight,
-    children,
-    ...restProps
-  }: LinkProps<T>,
-  ref?: PolymorphicRef<T>,
-) {
-  const Element = as || 'a';
-
-  const { focusProps, isFocusVisible } = useFocusRing();
-
-  return (
-    <Element
-      {...mergeProps(focusProps, restProps)}
-      ref={ref}
-      css={linkStyle({
-        inverted,
-        isFocusVisible,
-      })}
-    >
-      {iconLeft}
-      {
-        <span
-          css={innerContentStyle({
-            hasLeftIcon: !!iconLeft,
-            hasRightIcon: !!iconRight,
-          })}
-        >
-          {children}
-        </span>
-      }
-      {iconRight}
-    </Element>
-  );
-}
-
-const Link: LinkComponent = forwardRef(LinkBase);
+const Link: LinkComponent = forwardRef(LinkInternal);
 
 export default Link;
