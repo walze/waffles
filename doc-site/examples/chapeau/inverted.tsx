5e865d9827a7660734a236d08eed999c9d025efe
Mateusz Łachacz <info@blurbyte.com>
2021-10-18 17:01:28 +0200

    docs: add chapeau examples

---
+++ doc-site/examples/chapeau/inverted.tsx
@@ -0,0 +1,15 @@
+import { Chapeau } from '@datacamp/waffles/chapeau';
+import { Heading } from '@datacamp/waffles/heading';
+
+function Example() {
+  return (
+    <header>
+      <Chapeau inverted>Competitions</Chapeau>
+      <Heading inverted size="xxlarge">
+        Design a promo strategy for the insurance company
+      </Heading>
+    </header>
+  );
+}
+
+export default Example;
---
+++ doc-site/examples/chapeau/playground-config.ts
@@ -0,0 +1,28 @@
+import { Chapeau } from '@datacamp/waffles/chapeau';
+import { Heading } from '@datacamp/waffles/heading';
+
+import type { PlaygroundConfig } from '../../types';
+
+const initialCode = `
+import { Chapeau } from '@datacamp/waffles/chapeau';
+import { Heading } from '@datacamp/waffles/heading';
+
+function Playground() {
+  return (
+    <header>
+      <Chapeau>Certification</Chapeau>
+      <Heading size="xxlarge">Get Certified in Data Science</Heading>
+    </header>
+  );
+}
+`;
+
+const playgroundConfig: PlaygroundConfig = {
+  initialCode,
+  scope: {
+    Chapeau,
+    Heading,
+  },
+};
+
+export default playgroundConfig;
