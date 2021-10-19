c69fd5af7e1afd1e636fcb40ed5a53f8febd80ac
Mateusz Łachacz <info@blurbyte.com>
2021-10-19 11:28:04 +0200

    feat(code-block): update typings

--- src/code-block/code-block.tsx
+++ src/code-block/code-block.tsx
@@ -5,7 +5,14 @@ import { mergeProps } from '@react-aria/utils';
 import { Code } from '../code';
 import { codeBlockStyle } from './styles';
 
-type CodeBlockProps = React.ComponentProps<typeof Code>;
+type CodeBlockProps = {
+  /* The content of the code block. */
+  children: React.ReactNode;
+  /* Defines the font size of the code block. */
+  size?: 'medium' | 'large';
+  /* Sets the style of the code block suitable for dark backgrounds. */
+  inverted?: boolean;
+} & React.HTMLAttributes<HTMLPreElement>;
 
 function CodeBlock({ children, ...restProps }: CodeBlockProps) {
   const { focusProps, isFocusVisible } = useFocusRing();
