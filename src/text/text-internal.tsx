c4f20721f38dc78e42e400ecf309e02d212f1dae
Mateusz Łachacz <info@blurbyte.com>
2021-10-19 11:53:24 +0200

    docs: add inline text props documentation

---
+++ src/text/text-internal.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import type { PolymorphicRef, PolymorphicComponentProps } from '../helpers';
+import { textStyle } from './styles';
+
+type TextBaseProps = {
+  /* A text content of the component. */
+  children: React.ReactNode;
+};
+
+export type TextProps<T extends React.ElementType = 'span'> =
+  PolymorphicComponentProps<T, TextBaseProps>;
+
+function TextInternal<T extends React.ElementType = 'span'>(
+  { as, children, ...restProps }: TextProps<T>,
+  ref?: PolymorphicRef<T>,
+) {
+  const Element = as || 'span';
+
+  return (
+    <Element ref={ref} css={textStyle()} {...restProps}>
+      {children}
+    </Element>
+  );
+}
+
+export default TextInternal;
--- src/text/text.tsx
+++ src/text/text.tsx
@@ -1,32 +1,12 @@
 import React, { forwardRef } from 'react';
 
-import type { PolymorphicRef, PolymorphicComponentProps } from '../helpers';
-import { textStyle } from './styles';
+import TextInternal from './text-internal';
+import type { TextProps } from './text-internal';
 
-type TextBaseProps = {
-  children: React.ReactNode;
-};
-
-type TextProps<T extends React.ElementType = 'span'> =
-  PolymorphicComponentProps<T, TextBaseProps>;
-
-type TextComponent = <T extends React.ElementType = 'span'>(
+type LinkComponent = <T extends React.ElementType = 'a'>(
   props: TextProps<T>,
 ) => JSX.Element | null;
 
-function TextBase<T extends React.ElementType = 'span'>(
-  { as, children, ...restProps }: TextProps<T>,
-  ref?: PolymorphicRef<T>,
-) {
-  const Element = as || 'span';
-
-  return (
-    <Element ref={ref} css={textStyle()} {...restProps}>
-      {children}
-    </Element>
-  );
-}
-
-const Text: TextComponent = forwardRef(TextBase);
+const Text: LinkComponent = forwardRef(TextInternal);
 
 export default Text;
