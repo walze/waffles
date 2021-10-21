0bbed010f5dff20be051ba9bd1b908641694c9d2
Mateusz Łachacz <info@blurbyte.com>
2021-10-21 14:56:28 +0200

    docs: add inline content container props documentation

--- src/content-container/content-container.tsx
+++ src/content-container/content-container.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 import { contentContainerStyle } from './styles';
 
 type ContentContainerProps = {
+  /* The content to render inside the container. */
   children: React.ReactNode;
+  /* Hints whether there is a nav sidebar on the page or not. Changes how the container behaves. When the flag is passed, content is always centered, no matter what the viewport size is. */
   noSidebar?: boolean;
 } & React.HTMLAttributes<HTMLDivElement>;
 
