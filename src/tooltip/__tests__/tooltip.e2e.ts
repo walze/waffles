50baac8560b21bff2c7cf0b7b03537393c587471
Mateusz Łachacz <info@blurbyte.com>
2021-10-21 19:03:55 +0200

    test: update tooltip stories

--- src/tooltip/__tests__/tooltip.e2e.ts
+++ src/tooltip/__tests__/tooltip.e2e.ts
@@ -1,31 +1,31 @@
 describe('Tooltip', () => {
   it('renders a tooltip after trigger element got focused', () => {
     cy.loadStory('tooltip-basic');
-    cy.get('button').focus();
-    cy.findByRole('tooltip').should('exist');
+    cy.get('main').get('button').focus();
+    cy.get('[role=tooltip]').should('exist');
   });
 
   it('renders a tooltip after after mouse hovered over trigger', () => {
     cy.loadStory('tooltip-basic');
-    cy.get('button').trigger('mouseover');
-    cy.findByRole('tooltip').should('exist');
+    cy.get('main').get('button').trigger('mouseover');
+    cy.get('[role=tooltip]').should('exist');
   });
 
   it('renders an inverted tooltip', () => {
     cy.loadStory('tooltip-inverted');
-    cy.get('button').trigger('mouseover');
-    cy.findByRole('tooltip').should('exist');
+    cy.get('main').get('button').trigger('mouseover');
+    cy.get('[role=tooltip]').should('exist');
   });
 
   it('renders with custom styles applied', () => {
     cy.loadStory('tooltip-styled');
-    cy.get('button').trigger('mouseover');
-    cy.findByRole('tooltip').should('exist');
+    cy.get('main').get('button').trigger('mouseover');
+    cy.get('[role=tooltip]').should('exist');
   });
 
   it('renders a long content', () => {
     cy.loadStory('tooltip-long-content');
-    cy.get('button').trigger('mouseover');
-    cy.findByRole('tooltip').should('exist');
+    cy.get('main').get('button').trigger('mouseover');
+    cy.get('[role=tooltip]').should('exist');
   });
 });
