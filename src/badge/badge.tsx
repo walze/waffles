65ed02bacb6840e105bccf57907114517081fc59
Mateusz Łachacz <info@blurbyte.com>
2022-01-19 13:31:45 +0100

    docs: add Badge props documentation

--- src/badge/badge.tsx
+++ src/badge/badge.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import { badgeStyle, innerContentStyle } from './styles';
 
 type BadgeProps = {
+  /* A background color of the badge. Could be any [color](/foundation/design-tokens) from Waffles or a custom HEX color code (starts with hash followed by either 3 or 6 digits). */
   color: string;
+  /* The text content of the badge. */
+  children: React.ReactNode;
+  /* Defines the font size of the text content. In general default `medium` size should be used. */
   size?: 'small' | 'medium' | 'large';
 } & React.HTMLAttributes<HTMLSpanElement>;
 
