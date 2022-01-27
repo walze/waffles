7829ddcb21cea60ce9d182f3c74a8ce84d169a79
Mateusz Łachacz <info@blurbyte.com>
2022-01-27 08:59:49 +0100

    fix(side-navigation): remove scroll lock
    It was preventing from scrolling to the top of page in some cases

--- src/side-navigation/overlay.tsx
+++ src/side-navigation/overlay.tsx
@@ -1,11 +1,8 @@
-import { usePreventScroll } from '@react-aria/overlays';
-
 import { useSidebar } from './sidebar-context';
 import { overlayStyle } from './styles';
 
 function Overlay() {
   const { isOpen, onClose } = useSidebar();
-  usePreventScroll();
 
   return (
     <div
