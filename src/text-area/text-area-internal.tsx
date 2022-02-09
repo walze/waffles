c90d80f458e615167b3d90a7fbc79e5bbebf7396
Mateusz Łachacz <info@blurbyte.com>
2022-02-09 14:50:57 +0100

    feat(text-area): simplify TextArea to play nicely with FormField

--- src/text-area/text-area-internal.tsx
+++ src/text-area/text-area-internal.tsx
@@ -1,18 +1,11 @@
 import React, { useState } from 'react';
 
-import { FormControl } from '../form-control';
 import { textAreaWrapper, textAreaStyle } from './styles';
 import AutoGrow from './auto-grow';
 
 type TextAreaProps = {
-  /* The label displayed above the text area. */
-  label: string;
-  /* Optional description shown above the text area. */
-  description?: React.ReactNode;
-  /* Error message displayed below the text area. Also sets appropriate error styling of the text area itself, and `aria-invalid` attribute. */
-  error?: string;
-  /* Shows required indicator above the text area. */
-  required?: boolean;
+  /* Sets appropriate error style. */
+  error?: boolean;
   /* Sets the style of the text area suitable for dark backgrounds. */
   inverted?: boolean;
   /* Allows height to expand as much as it needs to in order to contain the current value. */
@@ -21,12 +14,8 @@ type TextAreaProps = {
 
 function TextAreaInternal(
   {
-    id,
-    label,
-    description,
     error,
     value,
-    required = false,
     inverted = false,
     autoGrow = false,
     onFocus,
@@ -52,25 +41,18 @@ function TextAreaInternal(
   }
 
   return (
-    <FormControl {...{ id, label, description, error, required, inverted }}>
-      {(formControlProps) => {
-        return (
-          <AutoGrow {...{ value, autoGrow }}>
-            <div css={textAreaWrapper({ isFocused, autoGrow })}>
-              <textarea
-                {...formControlProps}
-                {...restProps}
-                ref={ref}
-                value={value}
-                onFocus={handleFocus}
-                onBlur={handleBlur}
-                css={textAreaStyle({ hasError: !!error, inverted })}
-              />
-            </div>
-          </AutoGrow>
-        );
-      }}
-    </FormControl>
+    <AutoGrow {...{ value, autoGrow }}>
+      <div css={textAreaWrapper({ isFocused, autoGrow })}>
+        <textarea
+          {...restProps}
+          ref={ref}
+          value={value}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          css={textAreaStyle({ hasError: !!error, inverted })}
+        />
+      </div>
+    </AutoGrow>
   );
 }
 
