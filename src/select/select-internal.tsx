e0468a727621b8dd2ad392b58b7ccb06c76204b5
Mateusz Łachacz <info@blurbyte.com>
2022-02-09 14:50:41 +0100

    feat(select): simplify Select to play nicely with FormField

--- src/select/select-internal.tsx
+++ src/select/select-internal.tsx
@@ -1,20 +1,13 @@
 import React, { useState } from 'react';
 
 import { ChevronDown } from '../icon';
-import { FormControl } from '../form-control';
 import { selectWrapperStyle, selectStyle, iconRightStyle } from './styles';
 
 type SelectProps = {
-  /* The label displayed above the select. */
-  label: string;
   /* Options to be displayed in the dropdown. To show a placeholder provide an `option` with an empty value, and both `disabled` and `hidden` attributes set. */
   children?: React.ReactNode;
-  /* Optional description shown above the select. */
-  description?: React.ReactNode;
-  /* Error message displayed below the select. Also sets appropriate error styling of the text area itself, and `aria-invalid` attribute. */
-  error?: string;
-  /* Shows required indicator above the select. */
-  required?: boolean;
+  /* Sets appropriate error style. */
+  error?: boolean;
   /* Defines the size of the select. In most cases default `medium` size should be used. */
   size?: 'small' | 'medium' | 'large';
   /* Sets the style of the select suitable for dark backgrounds. */
@@ -23,11 +16,7 @@ type SelectProps = {
 
 function SelectInternal(
   {
-    id,
-    label,
-    description,
     error,
-    required,
     size = 'medium',
     inverted = false,
     disabled = false,
@@ -56,33 +45,26 @@ function SelectInternal(
   }
 
   return (
-    <FormControl {...{ id, label, description, error, required, inverted }}>
-      {(formControlProps) => {
-        return (
-          <div css={selectWrapperStyle({ size, disabled, isFocused })}>
-            <select
-              {...formControlProps}
-              {...restProps}
-              {...{ value, required, disabled }}
-              ref={ref}
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-              css={selectStyle({
-                hasError: !!error,
-                withPlaceholder: value === '' || value === 'placeholder',
-                size,
-                inverted,
-              })}
-            >
-              {children}
-            </select>
-            <div css={iconRightStyle({ size, inverted })}>
-              <ChevronDown />
-            </div>
-          </div>
-        );
-      }}
-    </FormControl>
+    <div css={selectWrapperStyle({ size, disabled, isFocused })}>
+      <select
+        {...restProps}
+        {...{ value, disabled }}
+        ref={ref}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        css={selectStyle({
+          hasError: !!error,
+          withPlaceholder: value === '' || value === 'placeholder',
+          size,
+          inverted,
+        })}
+      >
+        {children}
+      </select>
+      <div css={iconRightStyle({ size, inverted })}>
+        <ChevronDown />
+      </div>
+    </div>
   );
 }
 
