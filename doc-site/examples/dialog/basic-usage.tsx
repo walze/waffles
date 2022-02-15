55820271f5e65b4d923e96ddfb33d904687c9e83
Mateusz Łachacz <info@blurbyte.com>
2022-02-15 13:23:35 +0100

    docs: add Dialog examples

---
+++ doc-site/examples/dialog/basic-usage.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+import { Dialog } from '@datacamp/waffles/dialog';
+import { Paragraph } from '@datacamp/waffles/paragraph';
+import { Button } from '@datacamp/waffles/button';
+
+function Example() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)}>Open Dialog</Button>
+      <Dialog
+        aria-labelledby="case-study-dialog-header"
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
+        <Dialog.Header id="case-study-dialog-header">
+          Confirm Your Case Study Participation
+        </Dialog.Header>
+        <Dialog.Body>
+          <Paragraph>
+            To be able to participate in the case study, you will need a
+            computer with a webcam. If you fail to submit, you will have to wait
+            4 weeks before taking the case study again.
+          </Paragraph>
+          <Paragraph variant="secondary">
+            Support is available during regular European working
+            hours&mdash;Monday to Friday, CET 8:00 to 17:00.
+          </Paragraph>
+        </Dialog.Body>
+        <Dialog.Footer>
+          <Dialog.Button variant="secondary" onClick={() => setIsOpen(false)}>
+            Cancel
+          </Dialog.Button>
+          <Dialog.Button
+            autoFocus
+            varaint="primary"
+            onClick={() => setIsOpen(false)}
+          >
+            Confirm
+          </Dialog.Button>
+        </Dialog.Footer>
+      </Dialog>
+    </>
+  );
+}
+
+export default Example;
---
+++ doc-site/examples/dialog/playground-config.ts
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+
+import { Dialog } from '@datacamp/waffles/dialog';
+import { Paragraph } from '@datacamp/waffles/paragraph';
+import { Button } from '@datacamp/waffles/button';
+
+import type { PlaygroundConfig } from '../../types';
+
+const initialCode = `
+import { useState } from 'react';
+
+import { Dialog } from '@datacamp/waffles/dialog';
+import { Paragraph } from '@datacamp/waffles/paragraph';
+import { Button } from '@datacamp/waffles/button';
+
+function Playground() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)}>Open Dialog</Button>
+      <Dialog
+        aria-labelledby="basic-dialog-header"
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
+        <Dialog.Header id="basic-dialog-header">
+          Dialog Title
+        </Dialog.Header>
+        <Dialog.Body>
+          <Paragraph>
+            Main content of the dialog. Even very long content is supported.
+          </Paragraph>
+        </Dialog.Body>
+        <Dialog.Footer>
+          <Dialog.Button variant="secondary" onClick={() => setIsOpen(false)}>
+            Cancel
+          </Dialog.Button>
+          <Dialog.Button
+            autoFocus
+            varaint="primary"
+            onClick={() => setIsOpen(false)}
+          >
+            Confirm
+          </Dialog.Button>
+        </Dialog.Footer>
+      </Dialog>
+    </>
+  );
+}
+`;
+
+const playgroundConfig: PlaygroundConfig = {
+  initialCode,
+  scope: {
+    useState,
+    Dialog,
+    Paragraph,
+    Button,
+  },
+};
+
+export default playgroundConfig;
