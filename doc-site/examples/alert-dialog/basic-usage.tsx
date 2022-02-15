b0a77b44b8649fc16afeac1ead547a670e8ff5ff
Mateusz Łachacz <info@blurbyte.com>
2022-02-15 13:23:13 +0100

    docs: add AlertDialog examples

---
+++ doc-site/examples/alert-dialog/basic-usage.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+import { AlertDialog } from '@datacamp/waffles/alert-dialog';
+import { Paragraph } from '@datacamp/waffles/paragraph';
+import { Button } from '@datacamp/waffles/button';
+
+function Example() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)}>Open Alert Dialog</Button>
+      <AlertDialog
+        aria-labelledby="delete-exercise-alert-dialog-header"
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
+        <AlertDialog.Header id="delete-exercise-alert-dialog-header">
+          You Are About To Delete An Exercise
+        </AlertDialog.Header>
+        <AlertDialog.Body>
+          <Paragraph>
+            Are you sure you want to proceed with this action? This can not be
+            undone.
+          </Paragraph>
+        </AlertDialog.Body>
+        <AlertDialog.Footer>
+          <AlertDialog.Button
+            autoFocus
+            variant="secondary"
+            onClick={() => setIsOpen(false)}
+          >
+            Cancel
+          </AlertDialog.Button>
+          <AlertDialog.Button
+            variant="destructive"
+            onClick={() => setIsOpen(false)}
+          >
+            Delete
+          </AlertDialog.Button>
+        </AlertDialog.Footer>
+      </AlertDialog>
+    </>
+  );
+}
+
+export default Example;
---
+++ doc-site/examples/alert-dialog/playground-config.ts
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+
+import { AlertDialog } from '@datacamp/waffles/alert-dialog';
+import { Paragraph } from '@datacamp/waffles/paragraph';
+import { Button } from '@datacamp/waffles/button';
+
+import type { PlaygroundConfig } from '../../types';
+
+const initialCode = `
+import { useState } from 'react';
+
+import { AlertDialog } from '@datacamp/waffles/alert-dialog';
+import { Paragraph } from '@datacamp/waffles/paragraph';
+import { Button } from '@datacamp/waffles/button';
+
+function Playground() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)}>Open Alert Dialog</Button>
+      <AlertDialog
+        aria-labelledby="basic-alert-dialog-header"
+        aria-describedby="basic-alert-dialog-description"
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
+        <AlertDialog.Header id="basic-alert-dialog-header">
+          Alert Dialog Title
+        </AlertDialog.Header>
+        <AlertDialog.Body>
+          <Paragraph id="basic-alert-dialog-description">
+            Content of an alert dialog should be rather short.
+            This type of dialog is used to notify the user of urgent
+            information that demands immediate attention.
+          </Paragraph>
+        </AlertDialog.Body>
+        <AlertDialog.Footer>
+          <AlertDialog.Button
+            autoFocus
+            fullWidth
+            onClick={() => setIsOpen(false)}
+          >
+            Got It
+          </AlertDialog.Button>
+        </AlertDialog.Footer>
+      </AlertDialog>
+    </>
+  );
+}
+`;
+
+const playgroundConfig: PlaygroundConfig = {
+  initialCode,
+  scope: {
+    useState,
+    AlertDialog,
+    Paragraph,
+    Button,
+  },
+};
+
+export default playgroundConfig;
