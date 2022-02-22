0af7f9d3b63b8b58cbc29ac399c9ea9e70002829
Mateusz Łachacz <info@blurbyte.com>
2022-02-22 12:22:08 +0100

    docs: update TextArea documentation

---
+++ doc-site/examples/text-area/character-count.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+import { TextArea } from '@datacamp/waffles/text-area';
+
+function Example() {
+  const [value, setValue] = useState('');
+
+  function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setValue(event.target.value);
+  }
+
+  return (
+    <TextArea
+      showCharacterCount
+      maxLength={100}
+      placeholder="Text area with character count indicator."
+      rows={3}
+      value={value}
+      onChange={handleChange}
+    />
+  );
+}
+
+export default Example;
