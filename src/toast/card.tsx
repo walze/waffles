52a9bc732a90e0ed9bd0fa39b78ee968d0d863a1
Mateusz Łachacz <info@blurbyte.com>
2022-03-03 10:57:25 +0100

    feat(toast): extract Toast visual content to separate component

---
+++ src/toast/card.tsx
@@ -0,0 +1,65 @@
+import {
+  AttentionCircleInverted,
+  CheckmarkCircleInverted,
+  CrossCircleInverted,
+} from '../icon';
+import { Heading } from '../heading';
+import { Paragraph } from '../paragraph';
+import { ScreenReaderOnly } from '../screen-reader-only';
+import Toast from './toast';
+import CloseButton from './close-button';
+import {
+  toastStyle,
+  iconStyle,
+  contentStyle,
+  titleStyle,
+  descriptionStyle,
+} from './styles';
+
+type CardProps = React.ComponentProps<typeof Toast>;
+
+function Card({ onClose, title, variant = 'default', description }: CardProps) {
+  function renderIcon() {
+    switch (variant) {
+      case 'success':
+        return <CheckmarkCircleInverted />;
+      case 'error':
+        return <CrossCircleInverted />;
+      default:
+        return <AttentionCircleInverted />;
+    }
+  }
+
+  function renderAnnouncement() {
+    switch (variant) {
+      case 'success':
+        return <ScreenReaderOnly>Success:</ScreenReaderOnly>;
+      case 'warning':
+        return <ScreenReaderOnly>Warning:</ScreenReaderOnly>;
+      case 'error':
+        return <ScreenReaderOnly>Error:</ScreenReaderOnly>;
+      default:
+        return null;
+    }
+  }
+
+  return (
+    <section role="status" css={toastStyle({ variant })}>
+      <div css={iconStyle()}>{renderIcon()}</div>
+      <div css={contentStyle()}>
+        <Heading as="h2" size="medium" css={titleStyle()}>
+          {renderAnnouncement()}
+          {title}
+        </Heading>
+        {description && (
+          <Paragraph variant="secondary" css={descriptionStyle()}>
+            {description}
+          </Paragraph>
+        )}
+      </div>
+      <CloseButton onClick={onClose} />
+    </section>
+  );
+}
+
+export default Card;
