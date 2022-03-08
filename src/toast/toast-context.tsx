dbd70d25a9247779d3ce0f62fa62ca40fb999f6a
Mateusz Łachacz <info@blurbyte.com>
2022-03-08 18:01:49 +0100

    chore: split toast context and provider to make docs auto-generation possible

--- src/toast/toast-context.tsx
+++ src/toast/toast-context.tsx
@@ -1,18 +1,11 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { createContext, useContext, useState } from 'react';
-import { nanoid } from 'nanoid';
+import { createContext, useContext } from 'react';
 
-import ToastsList from './toasts-list';
 import Toast from './toast';
 
-type ToastOptions = Omit<
+export type ToastOptions = Omit<
   React.ComponentProps<typeof Toast>,
   'onClose' | 'disableAutoHide' | 'autoHideDuration'
->;
-
-type ToastsDataset = Record<
-  string,
-  Omit<React.ComponentProps<typeof Toast>, 'onClose'>
 >;
 
 type ToastContextValue = {
@@ -20,68 +13,6 @@ type ToastContextValue = {
 };
 
 const ToastContext = createContext<ToastContextValue>(undefined!);
-
-type ToastProviderProps = {
-  disableAutoHide?: boolean;
-  autoHideDuration?: number;
-  children: React.ReactNode;
-};
-
-function ToastProvider({
-  disableAutoHide = false,
-  autoHideDuration = 6000,
-  children,
-}: ToastProviderProps) {
-  // Keep each new toast in a hash map under unique ID
-  const [toasts, setToasts] = useState<ToastsDataset>({});
-  const toastIds = Object.keys(toasts);
-
-  // Create new toast, exposed by hook
-  function toast({ title, variant = 'default', description }: ToastOptions) {
-    const toastId = nanoid(6);
-
-    setToasts((toasts) => {
-      return {
-        ...toasts,
-        [toastId]: {
-          title,
-          variant,
-          description,
-          disableAutoHide,
-          autoHideDuration,
-        },
-      };
-    });
-  }
-
-  // Remove toast from hash map by ID
-  // Handler is called by toast after exit animation is finished
-  // It's called either automatically by internal toast timer or after close button is clicked
-  function handleClose(toastId: string) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { [toastId]: removed, ...restToasts } = toasts;
-    setToasts(restToasts);
-  }
-
-  return (
-    <ToastContext.Provider value={{ toast }}>
-      {toastIds.length > 0 && (
-        <ToastsList>
-          {toastIds.map((toastId) => {
-            return (
-              <Toast
-                key={toastId}
-                {...toasts[toastId]}
-                onClose={() => handleClose(toastId)}
-              />
-            );
-          })}
-        </ToastsList>
-      )}
-      {children}
-    </ToastContext.Provider>
-  );
-}
 
 function useToast() {
   const context = useContext(ToastContext);
@@ -93,4 +24,4 @@ function useToast() {
   return context;
 }
 
-export { ToastProvider, useToast };
+export { ToastContext, useToast };
---
+++ src/toast/toast-provider.tsx
@@ -0,0 +1,83 @@
+// Split context and provider to make docs auto-generation possible
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+import { ToastContext } from './toast-context';
+import type { ToastOptions } from './toast-context';
+import ToastsList from './toasts-list';
+import Toast from './toast';
+
+type ToastsDataset = Record<
+  string,
+  Omit<React.ComponentProps<typeof Toast>, 'onClose'>
+>;
+
+type ToastProviderProps = {
+  /* Turns off toasts auto-hide functionality. When flag is passed, notifications persist and must be dismissed manually. */
+  disableAutoHide?: boolean;
+  /* The number of milliseconds to wait before automatically dismissing a notification. */
+  autoHideDuration?: number;
+  /* Sets the distance from the top of the page to the list of toasts. By default most common app header height is used. */
+  offset?: string;
+  /* Content of an app. Generally it is enough to wrap the whole app close to its root with a single `ToastProvider`. */
+  children: React.ReactNode;
+};
+
+function ToastProvider({
+  disableAutoHide = false,
+  autoHideDuration = 6000,
+  offset = '54px',
+  children,
+}: ToastProviderProps) {
+  // Keep each new toast in a hash map under unique ID
+  const [toasts, setToasts] = useState<ToastsDataset>({});
+  const toastIds = Object.keys(toasts);
+
+  // Create new toast, exposed by hook
+  function toast({ title, variant = 'default', description }: ToastOptions) {
+    const toastId = nanoid(6);
+
+    setToasts((toasts) => {
+      return {
+        ...toasts,
+        [toastId]: {
+          title,
+          variant,
+          description,
+          disableAutoHide,
+          autoHideDuration,
+        },
+      };
+    });
+  }
+
+  // Remove toast from hash map by ID
+  // Handler is called by toast after exit animation is finished
+  // It's called either automatically by internal toast timer or after close button is clicked
+  function handleClose(toastId: string) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { [toastId]: removed, ...restToasts } = toasts;
+    setToasts(restToasts);
+  }
+
+  return (
+    <ToastContext.Provider value={{ toast }}>
+      {toastIds.length > 0 && (
+        <ToastsList offset={offset}>
+          {toastIds.map((toastId) => {
+            return (
+              <Toast
+                key={toastId}
+                {...toasts[toastId]}
+                onClose={() => handleClose(toastId)}
+              />
+            );
+          })}
+        </ToastsList>
+      )}
+      {children}
+    </ToastContext.Provider>
+  );
+}
+
+export default ToastProvider;
