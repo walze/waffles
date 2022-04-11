1392f66badb50df48fcc719462ad68872ed2f3b7
Mateusz Łachacz <info@blurbyte.com>
2022-04-11 16:34:12 +0200

    feat(content-container): removed noSidebar prop

--- src/content-container/content-container.tsx
+++ src/content-container/content-container.tsx
@@ -5,17 +5,11 @@ import { contentContainerStyle } from './styles';
 type ContentContainerProps = {
   /* The content to render inside the container. */
   children: React.ReactNode;
-  /* Hints whether there is a nav sidebar on the page or not. Changes how the container behaves. When the flag is passed, content is always centered, no matter what the viewport size is. */
-  noSidebar?: boolean;
 } & React.HTMLAttributes<HTMLDivElement>;
 
-function ContentContainer({
-  children,
-  noSidebar = false,
-  ...restProps
-}: ContentContainerProps) {
+function ContentContainer({ children, ...restProps }: ContentContainerProps) {
   return (
-    <div css={contentContainerStyle({ noSidebar })} {...restProps}>
+    <div css={contentContainerStyle()} {...restProps}>
       {children}
     </div>
   );
