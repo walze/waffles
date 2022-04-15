6e9d1f7f7393901570bda14a866e002926f7a871
Mateusz Łachacz <info@blurbyte.com>
2022-04-15 15:27:55 +0200

    feat(form-field): update children type to JSX.Element

--- src/form-field/form-field.tsx
+++ src/form-field/form-field.tsx
@@ -1,4 +1,4 @@
-import React, { Children, cloneElement, isValidElement } from 'react';
+import React, { cloneElement } from 'react';
 
 import { useId } from '../hooks';
 
@@ -11,7 +11,7 @@ import Description from './description';
 type FormFieldProps = {
   /* The label displayed above the input. */
   label: string;
-  /* Input component to be enhanced. In general pass Waffles [Input](/components/input), [TextArea](/components/text-area) or [Select](/components/select). */
+  /* Input component to be enhanced. In general pass Waffles [Input](/components/input), [TextArea](/components/text-area) or [Select](/components/select). Must be single element. */
   children: JSX.Element;
   /* Optional input identifier. Pass it when component is rendered on the server to avoid client-server attribute mismatch. */
   id?: string;
@@ -40,44 +40,39 @@ function FormField({
   const generatedId = useId('form-field');
   const fieldId = id || generatedId;
   const errorId = useId('form-field-error');
-  const formElement = Children.toArray(children)[0] as React.ReactElement; // It's safe because only 1 child is allowed
 
-  if (isValidElement(formElement)) {
-    const element = cloneElement(formElement as React.ReactElement, {
-      id: fieldId,
-      ...(required && { required }),
-      ...(inverted && { inverted }),
-      ...(error && {
-        error: true,
-        'aria-errormessage': errorId,
-        'aria-describedby': errorId,
-      }),
-    });
+  const element = cloneElement(children, {
+    id: fieldId,
+    ...(required && { required }),
+    ...(inverted && { inverted }),
+    ...(error && {
+      error: true,
+      'aria-errormessage': errorId,
+      'aria-describedby': errorId,
+    }),
+  });
 
-    return (
-      <div css={wrapperStyle()}>
-        <Label htmlFor={fieldId} inverted={inverted}>
-          {label}
-          <RequiredIndicator
-            isRequired={required}
-            indicator={requiredIndicator}
-            inverted={inverted}
-          />
-        </Label>
-        {description && (
-          <Description inverted={inverted}>{description}</Description>
-        )}
-        {element}
-        {error && (
-          <Error id={errorId} inverted={inverted}>
-            {error}
-          </Error>
-        )}
-      </div>
-    );
-  }
-
-  return children;
+  return (
+    <div css={wrapperStyle()}>
+      <Label htmlFor={fieldId} inverted={inverted}>
+        {label}
+        <RequiredIndicator
+          isRequired={required}
+          indicator={requiredIndicator}
+          inverted={inverted}
+        />
+      </Label>
+      {description && (
+        <Description inverted={inverted}>{description}</Description>
+      )}
+      {element}
+      {error && (
+        <Error id={errorId} inverted={inverted}>
+          {error}
+        </Error>
+      )}
+    </div>
+  );
 }
 
 export default FormField;
