545aaecef3c2311c790bc5a8f0f1948ad225ab42
Mateusz Łachacz <info@blurbyte.com>
2022-04-19 10:08:57 +0200

    feat(tooltip): prevent Tooltip from 'stealing' trigger ref

--- src/tooltip/tooltip.tsx
+++ src/tooltip/tooltip.tsx
@@ -87,6 +87,9 @@ function Tooltip({
   // @ts-expect-error: children.ref not recognized
   const triggerRef = useMergeRefs(reference, children.ref);
 
+  // @ts-expect-error: children.ref not recognized
+  const triggerRef = useMergeRefs(reference, children.ref);
+
   const element = cloneElement(
     children,
     getReferenceProps({
