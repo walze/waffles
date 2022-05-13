cdda48d9dc61257fcc9f1cedc084defe05c65d72
ixTec <christian.dalby@datacamp.com>
2022-05-13 13:57:39 +0100

    chore: remove todo comment

--- doc-site/components/all-asset-types.tsx
+++ doc-site/components/all-asset-types.tsx
@@ -3,8 +3,6 @@ import * as allAssets from '@datacamp/waffles/asset';
 
 import AssetGrid from './asset-grid';
 
-// TODO: Handle splitting assets up into their types (3D, assignment, misc etc) @ixTec
-
 function AllAssetTypes() {
   const groupedAssets = groupAssets(allAssets);
   return Object.entries(groupedAssets).map(([groupName, assetGroup]) => (
