32192a13785486714bc75074e210694871e8bde4
ixTec <christian.dalby@datacamp.com>
2022-05-17 10:21:50 +0200

    chore: add new badge to asset docs

--- doc-site/components/navigation.tsx
+++ doc-site/components/navigation.tsx
@@ -82,7 +82,9 @@ function Navigation({ isOpen, onClose }: NavigationProps) {
             <Item href="/components/toast">Toast</Item>
           </SideNav.Subcategory>
           <SideNav.Subcategory label="Content">
-            <Item href="/components/asset">Asset</Item>
+            <Item href="/components/asset" isNew>
+              Asset
+            </Item>
             <Item href="/components/badge">Badge</Item>
             <Item href="/components/content-container">Content Container</Item>
             <Item href="/components/icon">Icon</Item>
