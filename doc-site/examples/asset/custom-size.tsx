54cc3d6fd78558be6ea982f84e0528cf46ff11d2
ixTec <christian.dalby@datacamp.com>
2022-05-17 11:24:13 +0200

    docs(asset): update example and prop comment

---
+++ doc-site/examples/asset/custom-size.tsx
@@ -0,0 +1,7 @@
+import { DoubleArrow3d } from '@datacamp/waffles/asset';
+
+function Example() {
+  return <DoubleArrow3d width="200px" />;
+}
+
+export default Example;
--- src/asset/asset-internal.tsx
+++ src/asset/asset-internal.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 
 type AssetInternalProps = {
-  /* The width of the asset */
-  width?: string | number;
-  /* The height of the asset */
-  height?: string | number;
   /* [skip docs] */
   children: React.ReactNode;
 } & React.SVGAttributes<SVGElement>;
 
-function AssetInternal({
-  width,
-  height,
-  children,
-  ...restProps
-}: AssetInternalProps) {
+function AssetInternal({ children, ...restProps }: AssetInternalProps) {
   return (
-    <svg width={width} height={height} aria-hidden="true" {...restProps}>
+    <svg aria-hidden="true" {...restProps}>
       {children}
     </svg>
   );
