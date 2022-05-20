6478f87b05fcfede17e5b4c6297959b7bd7adb19
ixTec <christian.dalby@datacamp.com>
2022-05-20 12:05:59 +0200

    test: update expected test result

--- src/asset/__tests__/asset.e2e.ts
+++ src/asset/__tests__/asset.e2e.ts
@@ -1,6 +1,6 @@
 describe('Asset', () => {
   it('render all available assets', () => {
     cy.loadStory('asset-variants');
-    cy.findAllByTestId('asset-row').should('have.length', 35);
+    cy.findAllByTestId('asset-row').should('have.length', 66);
   });
 });
