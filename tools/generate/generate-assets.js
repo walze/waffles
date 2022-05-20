17c02bfbb7294ff110f64a7329b3529bd0e03e95
ixTec <christian.dalby@datacamp.com>
2022-05-20 11:52:26 +0200

    docs: add bundle of svg assets for download on docs

--- tools/generate/generate-assets.js
+++ tools/generate/generate-assets.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const glob = require('glob');
+const archiver = require('archiver');
 
 const { getOptimizedSvg } = require('../helpers/svg-generation');
 const {
@@ -19,12 +20,34 @@ const assetsDirPath = path.resolve(__dirname, '../../src/asset');
 const assetsExportDirPath = path.resolve(assetsDirPath, 'generated');
 const assetsExportPath = path.join(assetsDirPath, 'index.ts');
 
+const assetBundleExportPath = path.join(
+  __dirname,
+  '../../doc-site/public/waffles-asset-bundle.zip',
+);
+
+function generateAssetBundle() {
+  // Initialise output file for asset bundle and archiver
+  const assetOutputBundle = fs.createWriteStream(assetBundleExportPath);
+  const archive = archiver('zip');
+  archive.pipe(assetOutputBundle);
+  // Add all of the icons from the directory into the bundle zip, under group directory
+  archive.directory(path.join(assetsDirPath, 'raw'), false);
+
+  // Add the datacamp brand assets to the archive too
+  archive.directory(
+    path.join(__dirname, '../../src/branding-asset/raw'),
+    'datacamp-brand',
+  );
+  // Finalize the archiving
+  archive.finalize();
+}
+
 function generateAssets() {
   const assetDirs = glob.sync('*', { cwd: 'src/asset/raw/' });
   // Export key pairs
   const assetsExports = {};
 
-  // Iterate each asset input directory to handle the different type prefixes
+  // Iterate each asset input directory to handle the different types / groupings
   assetDirs.forEach((directory) => {
     // Array of SVG asset filenames
     const svgAssets = glob.sync('*.svg', { cwd: `src/asset/raw/${directory}` });
@@ -60,3 +83,4 @@ function generateAssets() {
 }
 
 generateAssets();
+generateAssetBundle();
