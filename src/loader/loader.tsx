dd8b9e1c812891d5214393738d6c75a0f01df911
ixTec <christian.dalby@datacamp.com>
2022-05-31 09:22:49 +0100

    refactor(loader): tidy up types and functions

--- src/loader/loader.tsx
+++ src/loader/loader.tsx
@@ -2,26 +2,23 @@ import React from 'react';
 
 import { tokens } from '../tokens';
 
-import { svgStyle, svgWrapperStyle, wrapperStyle } from './styles';
+import { loaderSvgStyle, loaderSvgWrapperStyle, wrapperStyle } from './styles';
 
 type LoaderProps = {
   /* Whether the loader is inverted in color or not. */
   inverted?: boolean;
 } & React.SVGAttributes<SVGElement>;
 
-const Loader = ({
-  inverted = false,
-  ...restProps
-}: LoaderProps): React.ReactElement => {
+function Loader({ inverted = false, ...restProps }: LoaderProps) {
   const { height, width } = restProps;
+
   return (
     <div
-      css={wrapperStyle(height as string, width as string)}
+      css={wrapperStyle({ height: height as string, width: width as string })}
       data-testid="loader-wrapper"
     >
-      <div css={svgWrapperStyle}>
-        <svg viewBox="0 0 2640 3444" css={svgStyle} {...restProps}>
-          <title>Loading</title>
+      <div css={loaderSvgWrapperStyle}>
+        <svg viewBox="0 0 2640 3444" css={loaderSvgStyle} {...restProps}>
           <path
             d="M0 0 M2569 1056L143 2447V149l1175 673v1867l1248 715"
             fill="none"
@@ -33,6 +30,6 @@ const Loader = ({
       </div>
     </div>
   );
-};
+}
 
 export default Loader;
--- src/loader/styles.ts
+++ src/loader/styles.ts
@@ -4,7 +4,12 @@ import { loaderEndMask, loaderStartMask, stroke } from './keyframes';
 
 const animationSettings = '2s infinite alternate';
 
-export function wrapperStyle(height: string, width: string) {
+type WrapperStyleProps = {
+  height: string;
+  width: string;
+};
+
+export function wrapperStyle({ height, width }: WrapperStyleProps) {
   return css`
     display: block;
     height: ${height};
@@ -15,12 +20,12 @@ export function wrapperStyle(height: string, width: string) {
   `;
 }
 
-export const svgWrapperStyle = css`
+export const loaderSvgWrapperStyle = css`
   animation: ${loaderEndMask} cubic-bezier(0, 0, 0.85, 1) ${animationSettings};
   will-change: clip-path;
 `;
 
-export const svgStyle = css`
+export const loaderSvgStyle = css`
   display: block;
   clip-path: polygon(-0.1% -10%, 169% 65%, -0.1% 139%);
   animation: ${stroke} cubic-bezier(0.65, 0, 0.55, 1) ${animationSettings};
