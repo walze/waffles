a4e10959c876e32d86a886c52e8cf289bc4b3111
ixTec <christian.dalby@datacamp.com>
2022-06-09 14:34:44 +0100

    test(asset): update tests for new assets

--- src/asset/__tests__/asset.e2e.ts
+++ src/asset/__tests__/asset.e2e.ts
@@ -1,6 +1,6 @@
 describe('Asset', () => {
   it('render all available assets', () => {
     cy.loadStory('asset-variants');
-    cy.findAllByTestId('asset-row').should('have.length', 79);
+    cy.findAllByTestId('asset-row').should('have.length', 86);
   });
 });
