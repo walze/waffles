9cb41682822d02825c3e429123fc53f11deb8aaf
ixTec <christian.dalby@datacamp.com>
2022-06-10 14:23:59 +0100

    chore: rename jest mocks folder

--- jest.config.js
+++ jest.config.js
@@ -16,6 +16,6 @@ module.exports = {
   snapshotSerializers: ['@emotion/jest/serializer'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      require.resolve('./jest/image-file-mock.js'),
+      require.resolve('./mocks/image-file-mock.js'),
   },
 };
