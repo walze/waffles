d43a2e2e580ca6a51dcb2d5d28c1e39ad0addcf5
ixTec <christian.dalby@datacamp.com>
2022-07-11 16:28:52 +0100

    chore: add animation to docs back to top button

--- doc-site/components/back-to-top.tsx
+++ doc-site/components/back-to-top.tsx
@@ -2,15 +2,27 @@ import { useEffect, useState } from 'react';
 import { css } from '@emotion/react';
 import { tokens } from '@datacamp/waffles/tokens';
 import { ArrowUp } from '@datacamp/waffles/icon';
+import { useAnimateTransition } from '@datacamp/waffles/hooks';
 import { Button } from '@datacamp/waffles/button';
 
 const buttonStyle = css`
   position: fixed;
   bottom: ${tokens.spacing.xlarge};
+  animation: fadeIn 1s;
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
 `;
 
 function BackToTop() {
   const [isVisible, setIsVisible] = useState(false);
+  const isAnimating = useAnimateTransition(isVisible, 300);
 
   useEffect(() => {
     // Show button if the window is scrolled below the default header height (54px)
@@ -26,17 +38,19 @@ function BackToTop() {
     };
   }, []);
 
-  return isVisible ? (
-    <Button
-      css={buttonStyle}
-      variant="plain"
-      size="small"
-      iconLeft={<ArrowUp />}
-      onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-    >
-      Back to Top
-    </Button>
-  ) : null;
+  return isVisible
+    ? isAnimating && (
+        <Button
+          css={buttonStyle}
+          variant="plain"
+          size="small"
+          iconLeft={<ArrowUp />}
+          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        >
+          Back to Top
+        </Button>
+      )
+    : null;
 }
 
 export default BackToTop;
