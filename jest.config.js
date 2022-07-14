6024e8e22726bc04190ea131518a753fbe2478fd
Mateusz Łachacz <info@blurbyte.com>
2022-07-14 13:27:29 +0200

    chore: revert updates to unit test setup

--- jest.config.js
+++ jest.config.js
@@ -9,13 +9,13 @@ module.exports = {
   rootDir: '.',
   testEnvironment: 'jsdom',
   coverageDirectory: './coverage',
-  setupFilesAfterEnv: ['<rootDir>/test-setup/initial-setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/build/', '/packages/'],
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   testRegex: '(.|-)spec\\.(js|ts|tsx|jsx)$',
   snapshotSerializers: ['@emotion/jest/serializer'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      require.resolve('./test-setup/image-file-mock.js'),
+      require.resolve('./mocks/image-file-mock.js'),
   },
 };
