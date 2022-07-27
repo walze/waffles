5e55987de5a36f48dcdd194cc6a58f23ecde4d44
Mateusz Łachacz <info@blurbyte.com>
2022-07-27 14:29:16 +0200

    feat(icon): add Scissors

---
+++ src/icon/generated/scissors.tsx
@@ -0,0 +1,20 @@
+// AUTO-GENERATED CONTENT - DO NOT MANUALLY EDIT - Run 'yarn generate:icons' to update
+
+import Icon from '../icon-internal';
+
+type ScissorsProps = Omit<React.ComponentProps<typeof Icon>, 'children'>;
+
+function Scissors({ size, ...restProps }: ScissorsProps) {
+  return (
+    <Icon viewBox="0 0 18 18" size={size} {...restProps}>
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M15.511 7.418a2 2 0 0 0 1.38-2.469L11.53 6.466l1.517-5.361a2 2 0 0 0-2.469 1.38l-1.307 4.62-2.242.634a3 3 0 1 0-2.003 2.646l3.606-1.02-1.049 3.705.005.002a3 3 0 1 0 2.666-2.096l.637-2.251 4.62-1.307zM4.409 8.48l-.003.002-.024.006.001.002a1 1 0 1 1 .026-.01zm5.115 5.115a1 1 0 1 0 1.855.75 1 1 0 0 0-1.855-.75z"
+      />
+    </Icon>
+  );
+}
+
+export default Scissors;
