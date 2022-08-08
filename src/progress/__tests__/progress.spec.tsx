97c1f5387e3545cbc2de385731a3522bab41afe0
ixTec <christian.dalby@datacamp.com>
2022-08-08 15:53:09 +0100

    test(progress): update spec tests and snapshots

--- src/progress/__tests__/progress.spec.tsx
+++ src/progress/__tests__/progress.spec.tsx
@@ -3,24 +3,34 @@ import { render } from '@testing-library/react';
 import { Progress } from '../index';
 
 describe('Progress', () => {
-  it('renders a progress bar', () => {
-    const { getByTestId } = render(<Progress value="40" max="100" />);
+  it('renders svg alongside progress when showSteps is true', () => {
+    const { getByTestId } = render(<Progress value={40} max={100} showSteps />);
+    const wrapper = getByTestId('progress-wrapper');
+    const svg = wrapper.querySelector('svg');
+    const progressEl = wrapper.querySelector('progress');
+
+    expect(svg).toBeInTheDocument();
+    expect(progressEl).toBeInTheDocument();
+  });
+
+  it('renders snapshot of a progress', () => {
+    const { getByTestId } = render(<Progress value={40} max={100} />);
     const progress = getByTestId('progress-wrapper');
 
     expect(progress).toBeInTheDocument();
     expect(progress).toMatchSnapshot();
   });
 
-  it('renders snapshot of an inverted progress bar', () => {
-    const { getByTestId } = render(<Progress inverted value="40" max="100" />);
+  it('renders snapshot of an inverted progress', () => {
+    const { getByTestId } = render(<Progress inverted value={40} max={100} />);
     const progress = getByTestId('progress-wrapper');
 
     expect(progress).toBeInTheDocument();
     expect(progress).toMatchSnapshot();
   });
 
-  it('renders snapshot of a progress bar with steps', () => {
-    const { getByTestId } = render(<Progress showSteps value="2" max="5" />);
+  it('renders snapshot of a progress with steps', () => {
+    const { getByTestId } = render(<Progress showSteps value={2} max={5} />);
     const progress = getByTestId('progress-wrapper');
 
     expect(progress).toBeInTheDocument();
