1a7dcae458af1194b550b482ae01b519e8d919d9
ixTec <christian.dalby@datacamp.com>
2022-08-08 16:00:56 +0100

    chore: revert workbench changes

--- workbench/workbench.tsx
+++ workbench/workbench.tsx
@@ -1,48 +1,10 @@
-import { css } from '@emotion/react';
-import { tokens } from '@datacamp/waffles/tokens';
-import { Progress } from '@datacamp/waffles/progress';
-
 function Workbench() {
   return (
     <>
-      <div
-        css={css`
-          padding: 32px;
-          background-color: white;
-          gap: ${tokens.spacing.small};
-          display: flex;
-          flex-direction: column;
-        `}
-      >
-        <Progress max={100} value={35} />
-        <Progress max={100} value={67} />
-        <Progress size="small" max={100} value={35} />
-        <Progress size="small" max={100} value={67} />
-
-        <Progress max={4} value={1} showSteps />
-        <Progress max={5} value={2} showSteps />
-        <Progress size="small" max={4} value={1} showSteps />
-        <Progress size="small" max={5} value={2} showSteps />
-      </div>
-      <div
-        css={css`
-          padding: 32px;
-          background-color: ${tokens.colors.navyDark};
-          display: flex;
-          flex-direction: column;
-          gap: ${tokens.spacing.small};
-        `}
-      >
-        <Progress inverted max={100} value={35} />
-        <Progress inverted max={100} value={67} />
-        <Progress inverted size="small" max={100} value={35} />
-        <Progress inverted size="small" max={100} value={67} />
-
-        <Progress inverted max={4} value={1} showSteps />
-        <Progress inverted max={5} value={2.5} showSteps />
-        <Progress inverted size="small" max={4} value={1} showSteps />
-        <Progress inverted size="small" max={5} value={2} showSteps />
-      </div>
+      {/*
+        Develop and test your components here.
+        IMPORTANT! Please do not commit your work to this file so it remains pristine for other developers.
+      */}
     </>
   );
 }
