c34894810b597b9dacf28802a54b88d7752c78c2
ixTec <christian.dalby@datacamp.com>
2022-08-10 13:49:28 +0100

    chore(progress): update prop docs

--- src/progress/progress.tsx
+++ src/progress/progress.tsx
@@ -9,7 +9,7 @@ import {
 import ProgressSteps from './steps';
 
 type ProgressProps = {
-  /* Optional identifier. Useful for when using your own `<label>`, for example. */
+  /* Optional identifier. Useful for when using your own `<label>`. */
   id?: string;
   /* The size of the progress. */
   size?: 'small' | 'medium';
@@ -17,7 +17,7 @@ type ProgressProps = {
   value?: number;
   /* The maximum value of the progress. Must be greater than `value`. The default is 100. */
   max?: number;
-  /* Whether the progress should be split into individual steps. If so, `max` and value` are treat as the total and current steps respectively. */
+  /* Whether the progress should be split into individual steps. If so, `max` and value` are treated as the total and current steps respectively. */
   showSteps?: boolean;
   /* Whether the progress is inverted in color or not. */
   inverted?: boolean;
