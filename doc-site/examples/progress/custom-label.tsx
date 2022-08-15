2e1307628c3067d0fe276caff676ce63ea10474c
ixTec <christian.dalby@datacamp.com>
2022-08-15 11:15:43 +0100

    docs(progress): update custom label example

--- doc-site/examples/progress/custom-label.tsx
+++ doc-site/examples/progress/custom-label.tsx
@@ -7,7 +7,7 @@ function Example() {
     <Progress
       value={current}
       aria-label="Progress with custom label"
-      customLabel={<span>{`${current}% complete`}</span>}
+      customLabel={`${current}% complete`}
     />
   );
 }
