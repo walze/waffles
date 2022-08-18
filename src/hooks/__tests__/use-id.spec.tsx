b4ab082f274409654ee5d2d4e08353408bf22ab6
Mateusz Łachacz <info@blurbyte.com>
2022-08-18 11:10:59 +0200

    test: update useId unit test

--- src/hooks/__tests__/use-id.spec.tsx
+++ src/hooks/__tests__/use-id.spec.tsx
@@ -3,20 +3,19 @@ import { render } from '@testing-library/react';
 import { useId } from '../index';
 
 function TestComponent() {
-  const customId = useId('custom-id');
-
+  const customId = useId();
   return <span id={customId}>Test</span>;
 }
 
 describe('useId', () => {
-  it('generates 6 characters long unique ID with provied prefix', () => {
+  it('generates 6 characters long unique ID ', () => {
     const { getByText } = render(<TestComponent />);
 
     const component = getByText('Test');
 
     expect(component).toHaveAttribute(
       'id',
-      expect.stringMatching(/^custom-id-[A-Za-z0-9-_]{6}$/),
+      expect.stringMatching(/^[A-Za-z0-9-_]{6}$/),
     );
   });
 });
