c1f45a894b155cb81f2795c1665eace8efdea5bc
Mateusz Łachacz <info@blurbyte.com>
2022-08-18 11:47:58 +0200

    docs: update useId description and example

--- doc-site/examples/hooks/use-id-basic-usage.tsx
+++ doc-site/examples/hooks/use-id-basic-usage.tsx
@@ -4,18 +4,23 @@ import { Text } from '@datacamp/waffles/text';
 import { Input } from '@datacamp/waffles/input';
 import { useId } from '@datacamp/waffles/hooks';
 
+const descriptionStyle = css`
+  margin-bottom: ${tokens.spacing.small};
+`;
+
 const errorStyle = css`
   color: ${tokens.colors.redDarkText};
   margin-top: ${tokens.spacing.small};
 `;
 
 function Example() {
-  const descriptionId = useId();
-  const errorId = useId('input-error');
+  const id = useId();
+  const descriptionId = `description-${id}`;
+  const errorId = `error-${id}`;
 
   return (
     <>
-      <Text id={descriptionId} as="p">
+      <Text id={descriptionId} as="p" css={descriptionStyle}>
         Description
       </Text>
       <Input
