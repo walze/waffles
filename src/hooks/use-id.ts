b7c0ff630cd2ee545b843e82d1fca9d734557254
Mateusz Łachacz <info@blurbyte.com>
2022-08-18 11:10:48 +0200

    feat(hooks): update useId implementation
    When available use React 18 useId, prefix got removed

--- src/hooks/use-id.ts
+++ src/hooks/use-id.ts
@@ -1,18 +1,49 @@
-import { useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
+
+import useIsomorphicLayoutEffect from './use-isomorphic-layout-effect';
+
+let serverHandoffComplete = false;
+
+function generateId() {
+  return nanoid(6);
+}
+
+function useGeneratedId() {
+  // If this instance isn't part of the initial render it doesn't have to double render
+  // Just generate the ID and return it
+  const initialId = serverHandoffComplete ? generateId() : null;
+  const [id, setId] = useState(initialId);
+
+  useIsomorphicLayoutEffect(() => {
+    // Patch the ID after render
+    // To avoid possible flickering useLayoutEffect is used
+    if (id == null) {
+      setId(generateId());
+    }
+  }, []);
+
+  useEffect(() => {
+    // Flag all future uses of useId to skip unnecessary re-renders
+    if (!serverHandoffComplete) {
+      serverHandoffComplete = true;
+    }
+  }, []);
+
+  return id;
+}
+
+// toString prevents bundlers from importing it
+// @ts-expect-error: might not exist
+const useReactId = React['useId'.toString()] as () => string;
 
 /**
  * Autogenerate random ID to facilitate accessibility features.
  *
- * For a components which are part of server-side initial render it may be required to provide IDs manually to avoid client-server values mismatch.
+ * When available use React 18 built-in `useId`, otherwise fall back to a bit less performant implementation.
  *
- * @param prefix Optional custom prefix for an ID
- * @returns Random ID with a prefix
+ * @returns Random ID
  */
-function useId(prefix?: string) {
-  const id = useMemo(() => nanoid(6), []);
-
-  return prefix == null ? id : `${prefix}-${id}`;
-}
+const useId = useReactId ?? useGeneratedId;
 
 export default useId;
