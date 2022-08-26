f845aadd0492c7db93a97155988ca87fdf9543ae
Mateusz Łachacz <info@blurbyte.com>
2022-08-26 11:04:17 +0200

    chore: update Code sizes story

--- src/code/__tests__/code-explicit-size.story.tsx
+++ src/code/__tests__/code-explicit-size.story.tsx
@@ -21,14 +21,14 @@ function Story() {
       <Paragraph>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut{' '}
-        <Code size="medium">inline code fragment of the small size</Code> enim
+        <Code size="medium">inline code fragment of the medium size</Code> enim
         ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
         aliquip ex ea commodo consequat.
       </Paragraph>
       <Paragraph>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut{' '}
-        <Code size="large">inline code fragment of the small size</Code> enim ad
+        <Code size="large">inline code fragment of the large size</Code> enim ad
         minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
         ex ea commodo consequat.
       </Paragraph>
